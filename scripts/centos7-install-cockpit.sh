21e11f5996e793d8063e8fe7a77d4c1e15308d68
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2018-08-29 15:12:58 -0500

    Create centos7-install-cockpit.sh

---
+++ scripts/centos7-install-cockpit.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Script to install Cockpit on CentOS 7. This script is not modular for RHEL 7 due to subscription-manager requirements
+# Created by: Keegan Jacobson
+# Based on: 
+#   https://cockpit-project.org/running#centos
+# Tested on CentOS 7
+# You can run it directly with curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/scripts/centos7-install-cockpit.sh | sudo bash
+
+# Install cockpit
+yum install cockpit
+
+# Enable cockpit
+systemctl enable --now cockpit.socket
+
+# Open firewall for cockpit
+firewall-cmd --permanent --zone=public --add-service=cockpit
+firewall-cmd --reload
