6ce49ffbd0d0d27c51e9587437726a105761369f
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2018-08-29 16:13:54 -0500

    Update centos7-install-cockpit.sh

--- scripts/centos7-install-cockpit.sh
+++ scripts/centos7-install-cockpit.sh
@@ -9,11 +9,16 @@
 # You can run it directly with curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/scripts/centos7-install-cockpit.sh | sudo bash
 
 # Install cockpit
-sudo yum -y install cockpit
+yum -y install cockpit
 
 # Enable cockpit
-sudo systemctl enable --now cockpit.socket
+systemctl enable --now cockpit.socket
 
 # Configure firewall
-sudo firewall-cmd --permanent --zone=public --add-service=cockpit
-sudo firewall-cmd --reload
+firewall-cmd --permanent --zone=public --add-service=cockpit
+firewall-cmd --reload
+
+# Fix https://github.com/cockpit-project/cockpit/issues/9193
+# Remove subscription-manager as it is not desirable, this should be able to be removed when CentOS 7.6 is released
+sudo sed -i 's/enabled=0/enabled=1/' /etc/yum/pluginconf.d/subscription-manager.conf /etc/yum/pluginconf.d/search-disabled-repos.conf /etc/yum/pluginconf.d/product-id.conf
+mv /etc/dbus-1/system.d/com.redhat.SubscriptionManager.conf{,.back}
