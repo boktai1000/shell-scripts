7b7ec24d9019af3c7d45c5260b285e6bfe3160ec
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2018-08-29 22:50:51 -0500

    Create centos7-install-cockpit.sh

---
+++ scripts/centos7-install-cockpit.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Script to install Cockpit on CentOS 7. This script is not modular for RHEL 7 due to subscription-manager requirements
+# Created by: Keegan Jacobson
+# Based on: 
+#   https://cockpit-project.org/running#centos
+#   https://www.vultr.com/docs/how-to-install-cockpit-on-centos-7
+# Tested on CentOS 7
+# You can run it directly with curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/scripts/centos7-install-cockpit.sh | sudo bash
+
+# Install cockpit
+yum -y install cockpit
+
+# Enable cockpit
+systemctl enable --now cockpit.socket
+
+# Configure firewall
+firewall-cmd --permanent --zone=public --add-service=cockpit
+firewall-cmd --reload
+
+# Fix https://github.com/cockpit-project/cockpit/issues/9193
+# Remove subscription-manager as it is not desirable, this should be able to be removed when CentOS 7.6 is released
+rpm -e --nodeps subscription-manager
