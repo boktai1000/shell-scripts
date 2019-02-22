5c2c33eb06dbf01d2321501ccbedb318c45acf87
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-02-21 21:20:32 -0600

    Update centos7-install-cockpit.sh
    Install sosreport functionality alongside cockpit to allow for Diagnostic Reports of the OS

--- scripts/centos7-install-cockpit.sh
+++ scripts/centos7-install-cockpit.sh
@@ -9,7 +9,7 @@
 # You can run it directly with curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/scripts/centos7-install-cockpit.sh | sudo bash
 
 # Install cockpit
-yum -y install cockpit
+yum -y install cockpit sos
 
 # Enable cockpit
 systemctl enable --now cockpit.socket
