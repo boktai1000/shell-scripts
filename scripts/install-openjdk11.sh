c27aaff9de697f7d5c947533050a53c17ed5bbd6
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-02-27 15:21:11 -0600

    Update install-openjdk11.sh
    Reload bash for user, bring one liner to its own line

--- scripts/install-openjdk11.sh
+++ scripts/install-openjdk11.sh
@@ -1,4 +1,5 @@
-# You can run it directly with curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/scripts/install-openjdk11.sh | sudo bash
+# You can run this script directly with the following command
+# curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/scripts/install-openjdk11.sh | sudo bash
 
 cd /tmp
 curl -O https://download.java.net/java/GA/jdk11/9/GPL/openjdk-11.0.2_linux-x64_bin.tar.gz
@@ -11,3 +12,6 @@ echo 'export PATH=$PATH:$JAVA_HOME/bin' >> /etc/profile.d/jdk11.sh
 
 source /etc/profile.d/jdk11.sh
 java -version
+
+cd
+exec bash
