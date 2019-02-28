1eb28d3642b79215a7dca0896cb30ffdb2fc025b
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-02-28 14:46:40 -0600

    Update centos7-install-cockpit.sh

--- scripts/centos7-install-cockpit.sh
+++ scripts/centos7-install-cockpit.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Script to install Cockpit on CentOS 7. This script is not modular for RHEL 7 due to subscription-manager requirements
-# Created by: Keegan Jacobson
-# Based on: 
-#   https://cockpit-project.org/running#centos
-#   https://www.vultr.com/docs/how-to-install-cockpit-on-centos-7
-# Tested on CentOS 7
 # You can run this script directly with the following command
 # curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/scripts/centos7-install-cockpit.sh | sudo bash
 
