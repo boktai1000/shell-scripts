e747881e97d083fbeea27cf6155d0391850c966d
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-02-27 23:38:47 -0600

    Update install-openjdk11-alt.sh

--- scripts/install-openjdk11-alt.sh
+++ scripts/install-openjdk11-alt.sh
@@ -1,3 +1,6 @@
+# You can run this script directly with the following command
+# curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/scripts/install-openjdk11-alt.sh | sudo bash
+
 cd /tmp
 curl -O https://download.java.net/java/GA/jdk11/9/GPL/openjdk-11.0.2_linux-x64_bin.tar.gz
 
