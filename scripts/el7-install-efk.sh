c95367614af0f78cec4eee8a8aacfd83ecd7c726
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-03-02 22:17:02 -0600

    Update el7-install-efk.sh

--- scripts/el7-install-efk.sh
+++ scripts/el7-install-efk.sh
@@ -1,3 +1,10 @@
+# Pre-reqs - Java 8 or OpenJDK 11
+# curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/scripts/install-openjdk11-alt.sh | sudo bash
+
+# You can run this script directly with the following command
+# curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/scripts/el7-install-efk.sh | sudo bash
+
+# Install Elasticsearch
 rpm --import https://artifacts.elastic.co/GPG-KEY-elasticsearch
 
 echo "[elasticsearch-6.x]
@@ -11,7 +18,24 @@ type=rpm-md" > /etc/yum.repos.d/elasticsearch.repo
 
 sudo yum install elasticsearch -y
 
-sudo systemctl daemon-reload
-sudo systemctl enable elasticsearch.service
+sudo /bin/systemctl daemon-reload
+sudo /bin/systemctl enable elasticsearch.service
 
 sudo systemctl start elasticsearch.service
+
+# Install Kibana
+echo "[kibana-6.x]
+name=Kibana repository for 6.x packages
+baseurl=https://artifacts.elastic.co/packages/6.x/yum
+gpgcheck=1
+gpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch
+enabled=1
+autorefresh=1
+type=rpm-md" > /etc/yum.repos.d/kibana.repo
+
+sudo yum install kibana -y
+
+sudo /bin/systemctl daemon-reload
+sudo /bin/systemctl enable kibana.service
+
+sudo systemctl start kibana.service
