56116a96a0fdea9b46fc5fddd1f03e4da5f02c5f
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-03-03 17:40:43 -0600

    Update and rename el7-install-efk.sh to el7-install-efk-server.sh

--- scripts/el7-install-efk-server.sh
+++ scripts/el7-install-efk-server.sh
@@ -2,7 +2,7 @@
 # curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/scripts/install-openjdk11-alt.sh | sudo bash
 
 # You can run this script directly with the following command
-# curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/scripts/el7-install-efk.sh | sudo bash
+# curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/scripts/el7-install-efk-server.sh | sudo bash
 
 # Install Elasticsearch
 rpm --import https://artifacts.elastic.co/GPG-KEY-elasticsearch
@@ -50,6 +50,29 @@ sudo systemctl start kibana.service
 
 # Test Elasticsearch
 curl -X GET http://localhost:9200
+
+
+# Pre-Fluentd Configuration
+
+cp /etc/security/limits.conf /etc/security/limits.conf.bak
+echo "root soft nofile 65536
+root hard nofile 65536
+* soft nofile 65536
+* hard nofile 65536" >> /etc/security/limits.conf
+
+cp /etc/sysctl.conf /etc/sysctl.conf.bak
+echo "net.core.somaxconn = 1024
+net.core.netdev_max_backlog = 5000
+net.core.rmem_max = 16777216
+net.core.wmem_max = 16777216
+net.ipv4.tcp_wmem = 4096 12582912 16777216
+net.ipv4.tcp_rmem = 4096 12582912 16777216
+net.ipv4.tcp_max_syn_backlog = 8096
+net.ipv4.tcp_slow_start_after_idle = 0
+net.ipv4.tcp_tw_reuse = 1
+net.ipv4.ip_local_port_range = 10240 65535" >> /etc/sysctl.conf
+
+sysctl -p
 
 # Install Fluentd (td-agent v3)
 
