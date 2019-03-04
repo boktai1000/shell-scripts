d5f7641d980473084d942842d440b37a4ac9ad7d
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-03-04 16:04:22 -0600

    Update linux-rsyslog-server-udp.sh

--- scripts/linux-rsyslog-server-udp.sh
+++ scripts/linux-rsyslog-server-udp.sh
@@ -4,12 +4,12 @@
 # Backup /etc/rsyslog.conf
 cp /etc/rsyslog.conf /etc/rsyslog.conf.bak
 
-# Configure the server to accept remote log messages using UDP
+# Configure Rsyslog to accept remote log messages using UDP port 514
 sed -i 's/#$ModLoad imudp/$ModLoad imudp/g' /etc/rsyslog.conf
 sed -i 's/#$UDPServerRun 514/$UDPServerRun 514/g' /etc/rsyslog.conf
 
 systemctl restart rsyslog
 
-# Open Firewall for Rsyslog to receive
+# Open firewall to receive remote log messages using UDP port 514
 firewall-cmd --permanent --add-port=514/udp
 firewall-cmd –reload
