cbb4c76f9f996e3f51fd135a097e2bcf5117bf2f
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-03-05 14:43:07 -0600

    Create el7-install-openjdk11.sh

---
+++ applications/el7-install-openjdk11.sh
@@ -0,0 +1,14 @@
+# You can run this script directly with the following command
+# curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/applications/el7-install-openjdk11.sh | sudo bash
+
+cd /tmp
+curl -O https://download.java.net/java/GA/jdk11/9/GPL/openjdk-11.0.2_linux-x64_bin.tar.gz
+
+tar zxvf openjdk-11.0.2_linux-x64_bin.tar.gz
+sudo mv jdk-11.0.2/ /usr/local/
+
+echo export JAVA_HOME=/usr/local/jdk-11.0.2 > /etc/profile.d/jdk11.sh
+echo 'export PATH=$PATH:$JAVA_HOME/bin' >> /etc/profile.d/jdk11.sh
+
+source /etc/profile.d/jdk11.sh
+java -version
