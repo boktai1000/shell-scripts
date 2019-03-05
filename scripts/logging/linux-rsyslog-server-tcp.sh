a07ee2165970a08f1b766dcd8b34f00945f4382a
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-03-05 14:26:15 -0600

    Create linux-rsyslog-server-tcp.sh

---
+++ scripts/logging/linux-rsyslog-server-tcp.sh
@@ -0,0 +1,15 @@
+# You can run this script directly with the following command
+# curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/scripts/logging/linux-rsyslog-server-tcp.sh | sudo bash
+
+# Backup /etc/rsyslog.conf
+cp /etc/rsyslog.conf /etc/rsyslog.conf.bak
+
+# Configure Rsyslog to accept remote log messages using TCP port 514
+sed -i 's/#$ModLoad imtcp/$ModLoad imtcp/g' /etc/rsyslog.conf
+sed -i 's/#$InputTCPServerRun 514/$InputTCPServerRun 514/g' /etc/rsyslog.conf
+
+systemctl restart rsyslog
+
+# Open firewall to receive remote log messages using TCP port 514
+firewall-cmd --permanent --add-port=514/tcp
+firewall-cmd –reload
