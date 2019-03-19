d68ff65fdfd23048f43c554b09162b74e09c4e6c
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-03-19 11:56:27 -0500

    Update el7-update-unifi.sh

--- applications/unifi/el7-update-unifi.sh
+++ applications/unifi/el7-update-unifi.sh
@@ -1,7 +1,7 @@
 #Based on https://community.ubnt.com/t5/UniFi-Wireless/Installing-UniFi-on-CentOS7-as-a-service/m-p/1973439/highlight/true#M234790
 
 #You can run this script directly with the following command
-#curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/applications/dfgdfg | sudo bash
+# curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/applications/unifi/el7-update-unifi.sh | sudo bash
 
 #Set Version (LTS Stable)
 unifiversion="$(curl -L --silent https://help.ubnt.com/hc/en-us/articles/360008240754#1 | grep -a1 "LTS Stable</td>" | egrep -o "([0-9]{1,}\.)+[0-9]{1,}")"
