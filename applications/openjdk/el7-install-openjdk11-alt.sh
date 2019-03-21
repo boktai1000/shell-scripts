d24a111d4c039227f1c5446213702fad13bf33e0
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-03-21 16:27:05 -0500

    Create el7-install-openjdk11-alt.sh

---
+++ applications/openjdk/el7-install-openjdk11-alt.sh
@@ -0,0 +1,11 @@
+# You can run this script directly with the following command
+# curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/applications/openjdk/el7-install-openjdk11-alt.sh | sudo bash
+
+cd /tmp
+curl -O https://download.java.net/java/GA/jdk11/9/GPL/openjdk-11.0.2_linux-x64_bin.tar.gz
+
+tar zxvf openjdk-11.0.2_linux-x64_bin.tar.gz
+sudo mv jdk-11.0.2/ /usr/local/
+
+alternatives --install /usr/bin/java java /usr/local/jdk-11.0.2/bin/java 1000
+alternatives --set java /usr/local/jdk-11.0.2/bin/java
