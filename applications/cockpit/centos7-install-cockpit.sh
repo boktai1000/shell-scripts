2309a921dae695118b6ae2882bc54791c9c95ccf
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-03-21 15:56:27 -0500

    Create centos7-install-cockpit.sh

---
+++ applications/cockpit/centos7-install-cockpit.sh
@@ -0,0 +1,12 @@
+# You can run this script directly with the following command
+# curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/applications/cockpit/centos7-install-cockpit.sh | sudo bash
+
+# Install cockpit
+yum -y install cockpit sos
+
+# Enable cockpit
+systemctl enable --now cockpit.socket
+
+# Configure firewall
+firewall-cmd --permanent --zone=public --add-service=cockpit
+firewall-cmd --reload
