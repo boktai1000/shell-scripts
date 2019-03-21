4f97b1e9fd6d3f1ddb4dbd6cbdf20350f694da8f
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-03-21 16:23:43 -0500

    Update el7-install-openjdk8.sh

--- applications/openjdk/el7-install-openjdk8.sh
+++ applications/openjdk/el7-install-openjdk8.sh
@@ -1,3 +1,4 @@
 # https://openjdk.java.net/install/
+# https://www.digitalocean.com/community/tutorials/how-to-install-java-on-centos-and-fedora#install-openjdk-8
 
-su -c "yum install java-1.8.0-openjdk"
+sudo yum install java-1.8.0-openjdk -y
