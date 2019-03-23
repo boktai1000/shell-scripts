32c667826d27ac4aac0b2408b36be16a67054f38
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-03-23 15:54:31 -0500

    Update linux-choose-tomcat.sh

--- applications/tomcat/linux-choose-tomcat.sh
+++ applications/tomcat/linux-choose-tomcat.sh
@@ -2,7 +2,7 @@
 # Append your version number after 'sudo bash -s' ex: 'sudo bash -s 9.0.17'
 # curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/applications/tomcat/linux-choose-tomcat.sh | sudo bash -s 
 
-# Set Variable to always download latest version of Tomcat 9 - Scrape Web Page for Version Number
+# Set variables to pass parameter / argument to script to grab version number from website
 tomcatmajorversion="`echo $1 | cut -c1-1`"
 tomcatminorversion="$1"
 
