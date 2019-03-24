7cc0e18db025102ae238357734cd5dcc8837a4cf
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-03-24 16:23:46 -0500

    Update ubuntu-install-cockpit.sh

--- applications/cockpit/ubuntu-install-cockpit.sh
+++ applications/cockpit/ubuntu-install-cockpit.sh
@@ -2,3 +2,8 @@
 # curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/applications/cockpit/ubuntu-install-cockpit.sh | sudo bash
 
 sudo apt-get install -y cockpit
+
+# Echo a reminder to CLI on how to login
+echo Login at your IP Address:
+hostname -I | awk '{print $1}'
+echo Default port: tcp/9090
