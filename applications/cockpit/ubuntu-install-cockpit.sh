96cce68b61f36df34c8e3d20e8e029378369c6f9
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-03-24 16:24:00 -0500

    Update ubuntu-install-cockpit.sh

--- applications/cockpit/ubuntu-install-cockpit.sh
+++ applications/cockpit/ubuntu-install-cockpit.sh
@@ -1,6 +1,7 @@
 # You can run this script directly with the following command
 # curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/applications/cockpit/ubuntu-install-cockpit.sh | sudo bash
 
+# Install Cockpit
 sudo apt-get install -y cockpit
 
 # Echo a reminder to CLI on how to login
