6f62de1c1688359f2d42a2a01bc7af41b8fb10cc
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-03-24 16:01:24 -0500

    Create ubuntu-install-cockpit.sh

---
+++ applications/cockpit/ubuntu-install-cockpit.sh
@@ -0,0 +1,4 @@
+# You can run this script directly with the following command
+# curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/applications/cockpit/ubuntu-install-cockpit.sh | sudo bash
+
+sudo apt-get install -y cockpit
