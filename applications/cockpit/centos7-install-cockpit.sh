a72bb4efc128fe34a2ba43eded15c135aabb43dd
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-03-24 16:24:29 -0500

    Update centos7-install-cockpit.sh

--- applications/cockpit/centos7-install-cockpit.sh
+++ applications/cockpit/centos7-install-cockpit.sh
@@ -10,3 +10,8 @@ systemctl enable --now cockpit.socket
 # Configure firewall
 firewall-cmd --permanent --zone=public --add-service=cockpit
 firewall-cmd --reload
+
+# Echo a reminder to CLI on how to login
+echo Login at your IP Address:
+hostname -I | awk '{print $1}'
+echo Default port: tcp/9090
