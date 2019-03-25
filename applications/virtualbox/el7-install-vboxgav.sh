27a84b7c43a0b99075f6c3e52d9c766b98475013
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-03-24 23:04:47 -0500

    Create el7-install-vboxgav.sh

---
+++ applications/virtualbox/el7-install-vboxgav.sh
@@ -0,0 +1,16 @@
+# You can run this script directly with the following command
+# curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/applications/virtualbox/el7-install-vboxgav.sh | sudo bash
+
+# Set VirtualBox Variable so argument from command line gets passed through
+VBOX_VERSION=$1
+
+sudo yum install -y bzip2
+
+cd /tmp
+curl -O http://download.virtualbox.org/virtualbox/$VBOX_VERSION/VBoxGuestAdditions_$VBOX_VERSION.iso
+
+mount -o loop,ro VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
+sh /mnt/VBoxLinuxAdditions.run
+
+umount /mnt
+rm /tmp/VBoxGuestAdditions_$VBOX_VERSION.iso
