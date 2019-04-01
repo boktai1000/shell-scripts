dd6c3275ac76e26c700b275da9a303e086f93178
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-03-31 22:10:50 -0500

    Create nfs-server-install.sh

---
+++ applications/nfs/nfs-server-install.sh
@@ -0,0 +1,19 @@
+
+# Install NFS Packages
+yum install -y nfs-utils
+
+# Enable services on reboot and start services
+systemctl enable rpcbind
+systemctl enable nfs-server
+systemctl enable nfs-lock
+systemctl enable nfs-idmap
+systemctl start rpcbind
+systemctl start nfs-server
+systemctl start nfs-lock
+systemctl start nfs-idmap
+
+# Open Firewall for NFS
+firewall-cmd --permanent --zone=public --add-service=nfs
+firewall-cmd --permanent --zone=public --add-service=mountd
+firewall-cmd --permanent --zone=public --add-service=rpc-bind
+firewall-cmd --reload
