6921f282adba5d36100646d2a4df119b6cc7de5d
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-04-01 15:48:13 -0500

    Create red-white_shell.sh

---
+++ customization/bash/red-white_shell.sh
@@ -0,0 +1,5 @@
+
+
+cat << EOF > /etc/profile.d/red-white_shell.sh
+export PS1="\[\e[01;37m\][\[\e[0m\]\[\e[01;31m\]\u\[\e[0m\]\[\e[00;37m\]@\[\e[0m\]\[\e[01;34m\]\h\[\e[0m\]\[\e[00;37m\] \[\e[0m\]\[\e[00;37m\]\t\[\e[0m\]\[\e[01;37m\] \W]\\$ \[\e[0m\]"
+EOF
