aefa81da320764c150ab0524f5a6216602b395d4
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-03-31 23:30:25 -0500

    Update nix-nfs-server-share.sh

--- applications/nfs/nix-nfs-server-share.sh
+++ applications/nfs/nix-nfs-server-share.sh
@@ -3,18 +3,18 @@
 # curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/applications/nfs/nix-nfs-server-share.sh | sudo bash -s 
 
 # Set variable for you to specify directory
-NFS_CLIENT_IP=$1
-NFS_SHARE_DIR=$2
+NFS_CLIENT=$1
+NFS_SHARE=$2
 
 # Create directory
-mkdir $NFS_SHARE_DIR
+mkdir $NFS_SHARE
 
 # Set permissions on NFS directory
-chmod -R 755 $NFS_SHARE_DIR
-chown nfsnobody:nfsnobody $NFS_SHARE_DIR
+chmod -R 755 $NFS_SHARE
+chown nfsnobody:nfsnobody $NFS_SHARE
 
 # Add directory to exports for sharing
-echo "$NFS_SHARE_DIR    $NFS_CLIENT_IP(rw,sync,no_root_squash,no_all_squash)" | sudo tee -a /etc/exports
+echo "$NFS_SHARE    $NFS_CLIENT(rw,sync,no_root_squash,no_all_squash)" | sudo tee -a /etc/exports
 
 # Restart NFS Service
 systemctl restart nfs-server
