093e3dfdbb5f64e4fb58971d77a987b059361e6b
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-04-01 15:50:01 -0500

    Create gold-silver_shell.sh

---
+++ customization/bash/gold-silver_shell.sh
@@ -0,0 +1,7 @@
+# You can run this script directly with the following command
+# curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/customization/bash/gold-silver_shell.sh | sudo bash
+
+# Color via http://bashrcgenerator.com/ (Custom)
+cat << EOF > /etc/profile.d/gold-silver_shell.sh
+export PS1="[\[$(tput sgr0)\]\[\033[38;5;220m\]\u\[$(tput sgr0)\]\[\033[38;5;15m\]@\[$(tput sgr0)\]\[\033[38;5;249m\]\h\[$(tput sgr0)\]\[\033[38;5;15m\] \T \W]\\$ \[$(tput sgr0)\]"
+EOF
