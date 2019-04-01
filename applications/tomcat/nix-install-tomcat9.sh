8562714e3a0148bc357abfe7f9430c8677e0603b
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-03-31 23:59:36 -0500

    Update and rename linux-install-tomcat9.sh to nix-install-tomcat9.sh

--- applications/tomcat/nix-install-tomcat9.sh
+++ applications/tomcat/nix-install-tomcat9.sh
@@ -1,5 +1,5 @@
 # You can run this script directly with the following command
-# curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/applications/tomcat/linux-install-tomcat9.sh | sudo bash
+# curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/applications/tomcat/nix-install-tomcat9.sh | sudo bash
 
 # Set Variable to always download latest version of Tomcat 9 - Scrape Web Page for Version Number
 tomcatversion="$(curl -s https://www-us.apache.org/dist/tomcat/tomcat-9/ | grep -Po '(?<=(<a href="v)).*(?=/">v)')"
