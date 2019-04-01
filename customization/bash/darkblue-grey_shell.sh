883a6f32e0dabc9249a914acbd5274bc4f671ff8
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-04-01 16:03:29 -0500

    Create darkblue-grey_shell.sh

---
+++ customization/bash/darkblue-grey_shell.sh
@@ -0,0 +1,7 @@
+# You can run this script directly with the following command
+# curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/customization/bash/darkblue-grey_shell.sh | sudo bash
+
+# Color via http://bashrcgenerator.com/ (Custom)
+cat << EOF > /etc/profile.d/darkblue-grey_shell.sh
+export PS1="[\[$(tput sgr0)\]\[\033[38;5;33m\]\u\[$(tput sgr0)\]\[\033[38;5;15m\]@\[$(tput sgr0)\]\[\033[38;5;249m\]\h\[$(tput sgr0)\]\[\033[38;5;15m\] \T \W]\\$ \[$(tput sgr0)\]"
+EOF
