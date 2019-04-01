ccd5a36e72c8f057d0e9800616896f2aab5fb0aa
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-03-31 23:29:49 -0500

    Update nix-nfs-client-share.sh

--- applications/nfs/nix-nfs-client-share.sh
+++ applications/nfs/nix-nfs-client-share.sh
@@ -3,17 +3,17 @@
 # curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/applications/nfs/nix-nfs-client-share.sh | sudo bash -s 
 
 # Set variables
-NFS_SERVER_IP=$1
-NFS_SHARE_DIR=$2
+NFS_SERVER=$1
+NFS_SHARE=$2
 
 # Create directory on filesystem where you will mount to
-mkdir -p /mnt/nfs$NFS_SHARE_DIR
+mkdir -p /mnt/nfs$NFS_SHARE
 
 # Add to fstab so mounted directory persists on reboot
-echo "$NFS_SERVER_IP:$NFS_SHARE_DIR    /mnt/nfs$NFS_SHARE_DIR   nfs defaults 0 0" | sudo tee -a /etc/fstab
+echo "$NFS_SERVER:$NFS_SHARE    /mnt/nfs$NFS_SHARE   nfs defaults 0 0" | sudo tee -a /etc/fstab
 
 # Attempt to mount new directory from fstab
 mount -a
 
 # Test the mount point
-touch /mnt/nfs$NFS_SHARE_DIR/test_nfs
+touch /mnt/nfs$NFS_SHARE/test_nfs
