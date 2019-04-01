26d2faad4d96cfb2b87902098fc9e60c578268d3
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-03-31 23:59:17 -0500

    Update and rename linux-latest-tomcat.sh to nix-latest-tomcat.sh

--- applications/tomcat/nix-latest-tomcat.sh
+++ applications/tomcat/nix-latest-tomcat.sh
@@ -1,5 +1,5 @@
 # You can run this script directly with the following command
-# curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/applications/tomcat/linux-latest-tomcat.sh | sudo bash
+# curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/applications/tomcat/nix-latest-tomcat.sh | sudo bash
 
 # Set Variable to find highest / latest version from GitHub and grab that version
 tomcatminorversion="$(curl -s https://api.github.com/repos/apache/tomcat/tags | grep '"name"' | head -1 | egrep -o "([0-9]{1,}\.)+[0-9]{1,}")"
