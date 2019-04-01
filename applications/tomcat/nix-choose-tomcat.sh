488ef849860347fe81a9a7c5e1ab2ed453508b07
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-03-31 22:58:58 -0500

    Update and rename linux-choose-tomcat.sh to nix-choose-tomcat.sh

--- applications/tomcat/nix-choose-tomcat.sh
+++ applications/tomcat/nix-choose-tomcat.sh
@@ -1,6 +1,6 @@
 # You can run this script directly with the following command
 # Append your version number after 'sudo bash -s' ex: 'sudo bash -s 9.0.17'
-# curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/applications/tomcat/linux-choose-tomcat.sh | sudo bash -s 
+# curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/applications/tomcat/nix-choose-tomcat.sh | sudo bash -s 
 
 # Set variables to pass parameter / argument to script to grab version number from website
 tomcatmajorversion="`echo $1 | cut -c1-1`"
