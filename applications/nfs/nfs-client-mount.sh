366ae57df47d18eedeaf351e0d2a22cbe91bd831
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-03-31 22:15:47 -0500

    Create nfs-client-mount.sh

---
+++ applications/nfs/nfs-client-mount.sh
@@ -0,0 +1,19 @@
+
+# Set variables
+NFS_SERVER_IP=$1
+NFS_SHARE_DIR=$2
+
+# Install required packages for mounting NFS shares
+yum install nfs-utils
+
+# Create directory on filesystem where you will mount to
+mkdir -p /mnt/nfs$NFS_SHARE_DIR
+
+# Add to fstab so mounted directory persists on reboot
+echo "$NFS_SERVER_IP:$NFS_SHARE_DIR    /mnt/nfs$NFS_SHARE_DIR   nfs defaults 0 0" | sudo tee -a /etc/fstab
+
+# Attempt to mount new directory from fstab
+mount -a
+
+# Test the mount point
+touch /mnt/nfs$NFS_SHARE_DIR/test_nfs
