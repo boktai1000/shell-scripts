d68de1d3fa855dc8dc4a48dec1c09e16a082d415
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-03-31 23:05:14 -0500

    Update el7-nfs-client-install.sh

--- applications/nfs/el7-nfs-client-install.sh
+++ applications/nfs/el7-nfs-client-install.sh
@@ -1,4 +1,4 @@
 # curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/applications/nfs/el7-nfs-client-install.sh | sudo bash
 
-# Install required packages for mounting NFS shares
+# Install NFS Packages
 yum install -y nfs-utils
