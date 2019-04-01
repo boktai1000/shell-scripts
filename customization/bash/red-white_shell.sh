efbca98aeceb201a1cd19e834702bd2964587682
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-04-01 15:49:06 -0500

    Update red-white_shell.sh

--- customization/bash/red-white_shell.sh
+++ customization/bash/red-white_shell.sh
@@ -1,5 +1,7 @@
+# You can run this script directly with the following command
+# curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/customization/bash/red-white_shell.sh | sudo bash
 
-
+# Color via https://askubuntu.com/a/549150 (Server root)
 cat << EOF > /etc/profile.d/red-white_shell.sh
 export PS1="\[\e[01;37m\][\[\e[0m\]\[\e[01;31m\]\u\[\e[0m\]\[\e[00;37m\]@\[\e[0m\]\[\e[01;34m\]\h\[\e[0m\]\[\e[00;37m\] \[\e[0m\]\[\e[00;37m\]\t\[\e[0m\]\[\e[01;37m\] \W]\\$ \[\e[0m\]"
 EOF
