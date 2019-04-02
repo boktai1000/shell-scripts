464bb1c1f2874360f8040714efb22172d569839a
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-04-02 12:38:34 -0500

    Delete source-test.sh

--- applications/vim/source-test.sh
+++
@@ -1,5 +0,0 @@
-# You can run this script directly with the following command
-# curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/applications/vim/source-test.sh | sudo bash
-
-# Reload profile / environment variables
-. /etc/profile
