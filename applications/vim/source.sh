2f380ee31c1b492ba70a83b44ca53b906c851c33
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-04-02 12:38:40 -0500

    Delete source.sh

--- applications/vim/source.sh
+++
@@ -1,5 +0,0 @@
-# You can run this script directly with the following command
-# curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/applications/vim/source.sh | sudo bash
-
-# Reload profile / environment variables
-. /etc/profile
