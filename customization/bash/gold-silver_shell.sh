68c180aa2b483c0b47fc289a7d030c3f17b05260
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-04-02 14:26:49 -0500

    Update gold-silver_shell.sh

--- customization/bash/gold-silver_shell.sh
+++ customization/bash/gold-silver_shell.sh
@@ -1,7 +1,7 @@
 # You can run this script directly with the following command
 # source <(curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/customization/bash/gold-silver_shell.sh)
 
-# Color via http://bashrcgenerator.com/ (Custom)
+# Color via http://bashrcgenerator.com/ (Keegan Custom)
 sudo bash -c 'cat > /etc/profile.d/gold-silver_shell.sh' << EOF
 export PS1="[\[\]\[\033[38;5;220m\]\u\[\]\[\033[38;5;15m\]@\[\]\[\033[38;5;249m\]\h\[\]\[\033[38;5;15m\] \T \W]\$ \[\]"
 EOF
