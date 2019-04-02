4946848f38672b2b4d82fc3070c755cd5cbd0c7b
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-04-02 10:40:57 -0500

    Create source.sh

---
+++ applications/vim/source.sh
@@ -0,0 +1,5 @@
+# You can run this script directly with the following command
+# curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/applications/vim/source.sh | sudo bash
+
+# Reload profile / environment variables
+source /etc/profile
