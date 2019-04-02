43de162ac05ccfbfc750d1e055deb76acb271c63
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-04-02 14:28:22 -0500

    Update blue-grey_shell.sh

--- customization/bash/blue-grey_shell.sh
+++ customization/bash/blue-grey_shell.sh
@@ -1,7 +1,10 @@
 # You can run this script directly with the following command
 # source <(curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/customization/bash/blue-grey_shell.sh)
 
-# Color via http://bashrcgenerator.com/ (Custom)
-cat << EOF > /etc/profile.d/blue-grey_shell.sh
+# Color via http://bashrcgenerator.com/ (Keegan Custom)
+sudo bash -c 'cat > /etc/profile.d/blue-grey_shell.sh' << EOF
 export PS1="[\[$(tput sgr0)\]\[\033[38;5;39m\]\u\[$(tput sgr0)\]\[\033[38;5;15m\]@\[$(tput sgr0)\]\[\033[38;5;249m\]\h\[$(tput sgr0)\]\[\033[38;5;15m\] \T \W]\\$ \[$(tput sgr0)\]"
 EOF
+
+# Load new bash settings
+. /etc/profile.d/blue-grey_shell.sh
