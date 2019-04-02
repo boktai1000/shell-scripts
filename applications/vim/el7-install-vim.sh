dc6278e5f1b87607e5d259138b5c30f7705474e4
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-04-02 10:18:29 -0500

    Create el7-install-vim.sh

---
+++ applications/vim/el7-install-vim.sh
@@ -0,0 +1,5 @@
+# You can run this script directly with the following command
+# curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/applications/vim/el7-install-vim.sh | sudo bash
+
+# Install VIM - supersedes VI
+sudo yum install -y vim
