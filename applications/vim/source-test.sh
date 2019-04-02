d8e056c774065d808d1afc12c59cfb4638ae3b56
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-04-02 10:43:23 -0500

    Create source-test.sh

---
+++ applications/vim/source-test.sh
@@ -0,0 +1,5 @@
+# You can run this script directly with the following command
+# curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/applications/vim/source-test.sh | sudo bash
+
+# Reload profile / environment variables
+. /etc/profile
