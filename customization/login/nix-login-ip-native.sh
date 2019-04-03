dde020e6e993db6af909202e42f1db7f2338c7ab
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-04-03 14:53:36 -0500

    Update and rename el7-login-ip-native.sh to nix-login-ip-native.sh

--- customization/login/nix-login-ip-native.sh
+++ customization/login/nix-login-ip-native.sh
@@ -1,5 +1,5 @@
 # You can run this script directly with the following command
-# curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/customization/login/el7-login-ip-native.sh | sudo bash
+# curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/customization/login/nix-login-ip-native.sh | sudo bash
 
 # Backup /etc/issue
 sudo cp /etc/issue /etc/issue.bak
