33279ca2c5bf64f2334090c6940e545815db25c6
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-04-03 14:52:57 -0500

    Create el7-login-ip-native.sh

---
+++ customization/login/el7-login-ip-native.sh
@@ -0,0 +1,11 @@
+# You can run this script directly with the following command
+# curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/customization/login/el7-login-ip-native.sh | sudo bash
+
+# Backup /etc/issue
+sudo cp /etc/issue /etc/issue.bak
+
+# Append Local IP Address to /etc/issue file
+sudo tee -a /etc/issue <<EOF
+Local IP addresses: \4
+
+EOF
