2be66c909a53626b287a058abd44a92e80b38eb1
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-04-07 18:56:18 -0500

    Update tomcat-variable-test.sh

--- applications/tomcat/testing/tomcat-variable-test.sh
+++ applications/tomcat/testing/tomcat-variable-test.sh
@@ -1,3 +1,7 @@
+# You can run this script directly with the following command
+# Append your version number followed by desired port number after 'sudo bash -s' ex: 'sudo bash -s 9.0.17 9090'
+# curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/applications/tomcat/testing/tomcat-variable-test.sh | sudo bash -s 
+
 tomcatmajorversion="`echo $1 | cut -c1-1`"
 tomcatminorversion="$1"
 yourip=$(hostname -I | awk '{print $1}')
