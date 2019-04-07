806bc156dd62d3a4794e4d20e0459f0dd1888000
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-04-07 18:46:21 -0500

    Update nix-choose-tomcat-port.sh

--- applications/tomcat/nix-choose-tomcat-port.sh
+++ applications/tomcat/nix-choose-tomcat-port.sh
@@ -1,6 +1,6 @@
 # You can run this script directly with the following command
 # Append your version number after 'sudo bash -s' ex: 'sudo bash -s 9.0.17'
-# curl -s  | sudo bash -s 
+# curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/applications/tomcat/nix-choose-tomcat-port.sh | sudo bash -s 
 
 # Set variables to pass parameter / argument to script to grab version number from website
 tomcatmajorversion="`echo $1 | cut -c1-1`"
