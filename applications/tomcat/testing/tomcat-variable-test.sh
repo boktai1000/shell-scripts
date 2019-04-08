7bc3f6a59ab068175017c7b6f0dc3724e383aa06
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-04-08 11:35:52 -0500

    Update tomcat-variable-test.sh

--- applications/tomcat/testing/tomcat-variable-test.sh
+++ applications/tomcat/testing/tomcat-variable-test.sh
@@ -2,8 +2,9 @@
 # Append your version number followed by desired port number after 'sudo bash -s' ex: 'sudo bash -s 9.0.17 9090'
 # curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/applications/tomcat/testing/tomcat-variable-test.sh | sudo bash -s 
 
-tomcatmajorversion="`echo $1 | cut -c1-1`"
-tomcatminorversion="$1"
+tomcatlatest="$(curl -s https://api.github.com/repos/apache/tomcat/tags | grep '"name"' | head -1 | egrep -o "([0-9]{1,}\.)+[0-9]{1,}")"
+tomcatminorversion=${1:-$tomcatlatest}
+tomcatmajorversion="`echo $tomcatminorversion | cut -c1-1`"
 yourip=$(hostname -I | awk '{print $1}')
 tomcatport="${2:-8080}"
 
@@ -15,6 +16,7 @@ echo "Your Major Version $tomcatmajorversion"
 echo "Your Minor Version $tomcatminorversion"
 echo "Your IP address $yourip"
 echo "Your Tomcat Port $tomcatport"
+echo "Tomcat Latest $tomcatlatest"
 cat /tmp/8080testfile
 
 rm /tmp/8080testfile
