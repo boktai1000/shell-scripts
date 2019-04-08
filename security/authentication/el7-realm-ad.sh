d21a16fe5088087f1d5dc6432b6860f2ff2a4017
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-04-08 16:47:20 -0500

    Update el7-realm-ad.sh

--- security/authentication/el7-realm-ad.sh
+++ security/authentication/el7-realm-ad.sh
@@ -1,4 +1,5 @@
 # https://access.redhat.com/documentation/en-us/red_hat_enterprise_linux/7/html-single/windows_integration_guide/index#sssd-ad-proc
+# Note to authenticate you need to do "su - user@DOMAIN.TLD" or ssh using "user@DOMAIN.TLD"
 
 yum install -y realmd oddjob oddjob-mkhomedir sssd samba-common-tools
 
