154a7beec96eaac82f4da94717ad7011de52734e
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-04-08 16:18:37 -0500

    Update el7-sssd-ad.sh

--- security/authentication/el7-sssd-ad.sh
+++ security/authentication/el7-sssd-ad.sh
@@ -1,7 +1,7 @@
 # https://access.redhat.com/articles/3023951
 # Note: Hostname must be 15 characters or less
 
-# You can run this script directly with the following command, append domain name followed by domain administrator password
+# You can run this script directly with the following command, append domain name, workgroup, and then Admin password in that order
 # curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/security/authentication/el7-sssd-ad.sh | sudo bash -s 
 
 # Install Required Packages for SSSD
@@ -23,8 +23,49 @@ echo "[logging]
  rdns = false
  forwardable = yes" > /etc/krb5.conf
 
+# Backup Samba configuration file
+cp /etc/samba/smb.conf /etc/samba/smb.conf.bak
+
+# Configure the Samba server to connect to the AD server.
+echo "# See smb.conf.example for a more detailed config file or
+# read the smb.conf manpage.
+# Run 'testparm' to verify the config is correct after
+# you modified it.
+
+[global]
+        workgroup = $2
+        client signing = yes
+        client use spnego = yes
+        kerberos method = secrets and keytab
+        log file = /var/log/samba/%m.log
+        password server = $1
+        realm = $1
+        security = ads
+
+[homes]
+        comment = Home Directories
+        valid users = %S, %D%w%S
+        browseable = No
+        read only = No
+        inherit acls = Yes
+
+[printers]
+        comment = All Printers
+        path = /var/tmp
+        printable = Yes
+        create mask = 0600
+        browseable = No
+
+[print$]
+        comment = Printer Drivers
+        path = /var/lib/samba/drivers
+        write list = @printadmin root
+        force group = @printadmin
+        create mask = 0664
+        directory mask = 0775" > /etc/samba/smb.conf
+
 # Obtain Kerberos credentials for a Windows administrative user
-echo $2 | kinit Administrator
+echo $3 | kinit Administrator
 
 # Add machine to domain
 net ads join -k
