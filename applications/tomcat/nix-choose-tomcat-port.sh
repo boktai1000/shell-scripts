d39c0b4782a36434f39fa0e5a6e5233451277639
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-04-07 20:24:28 -0500

    Update nix-choose-tomcat-port.sh

--- applications/tomcat/nix-choose-tomcat-port.sh
+++ applications/tomcat/nix-choose-tomcat-port.sh
@@ -8,21 +8,21 @@ tomcatminorversion="$1"
 yourip=$(hostname -I | awk '{print $1}')
 tomcatport="${2-8080}"
 
-cd /tmp
-curl -O https://archive.apache.org/dist/tomcat/tomcat-$tomcatmajorversion/v$tomcatminorversion/bin/apache-tomcat-$tomcatminorversion.tar.gz
-tar -xzvf apache-tomcat-$tomcatminorversion.tar.gz -C /opt
-
-cd /opt
-mv apache-tomcat-$tomcatminorversion tomcat$tomcatmajorversion-$tomcatport
-
-cd /opt/tomcat$tomcatmajorversion-$tomcatport/webapps/
-sudo rm -rf docs examples manager host-manager
-sed -i "s/8080/$tomcatport/g" /opt/tomcat$tomcatmajorversion-$tomcatport/conf/server.xml
-
 groupadd tomcat
 useradd -g tomcat -d /opt/tomcat -s /bin/nologin tomcat
 
-chown -R tomcat:tomcat /opt/tomcat$tomcatmajorversion-$tomcatport/
+cd /tmp
+curl -O https://archive.apache.org/dist/tomcat/tomcat-$tomcatmajorversion/v$tomcatminorversion/bin/apache-tomcat-$tomcatminorversion.tar.gz
+tar -xzvf apache-tomcat-$tomcatminorversion.tar.gz -C /opt/tomcat
+
+cd /opt/tomcat
+mv apache-tomcat-$tomcatminorversion tomcat$tomcatmajorversion-$tomcatport
+
+cd /opt/tomcat/tomcat$tomcatmajorversion-$tomcatport/webapps/
+sudo rm -rf docs examples manager host-manager
+sed -i "s/8080/$tomcatport/g" /opt/tomcat/tomcat$tomcatmajorversion-$tomcatport/conf/server.xml
+
+chown -R tomcat:tomcat /opt/tomcat/tomcat$tomcatmajorversion-$tomcatport/
 
 firewall-cmd --zone=public --permanent --add-port=$tomcatport/tcp
 firewall-cmd --reload
@@ -35,14 +35,14 @@ After=network.target
 Type=forking
 
 Environment=JAVA_HOME=/usr/local/jdk-11.0.2
-Environment=CATALINA_PID=/opt/tomcat$tomcatmajorversion-$tomcatport/temp/tomcat.pid
-Environment=CATALINA_HOME=/opt/tomcat$tomcatmajorversion-$tomcatport
-Environment=CATALINA_BASE=/opt/tomcat$tomcatmajorversion-$tomcatport
+Environment=CATALINA_PID=/opt/tomcat/tomcat$tomcatmajorversion-$tomcatport/temp/tomcat.pid
+Environment=CATALINA_HOME=/opt/tomcat/tomcat$tomcatmajorversion-$tomcatport
+Environment=CATALINA_BASE=/opt/tomcat/tomcat$tomcatmajorversion-$tomcatport
 Environment='CATALINA_OPTS=-Xms512M -Xmx1024M -server -XX:+UseParallelGC'
 Environment='JAVA_OPTS=-Djava.awt.headless=true -Djava.security.egd=file:/dev/./urandom'
 
-ExecStart=/opt/tomcat$tomcatmajorversion-$tomcatport/bin/startup.sh
-ExecStop=/opt/tomcat$tomcatmajorversion-$tomcatport/bin/shutdown.sh
+ExecStart=/opt/tomcat/tomcat$tomcatmajorversion-$tomcatport/bin/startup.sh
+ExecStop=/opt/tomcat/tomcat$tomcatmajorversion-$tomcatport/bin/shutdown.sh
 
 User=tomcat
 Group=tomcat
@@ -58,4 +58,4 @@ systemctl enable tomcat$tomcatmajorversion-$tomcatport
 systemctl start tomcat$tomcatmajorversion-$tomcatport
 
 # Echo a reminder to CLI on how to connect to Tomcat
-echo Connect to Tomcat at https://$yourip:$tomcatport
+echo Connect to Tomcat at http://$yourip:$tomcatport
