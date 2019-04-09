eff2fe1447584294fc4572e27af0625dd5b832c3
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-04-09 13:06:49 -0500

    Update el7-install-openvmtools.sh

--- applications/vmware/el7-install-openvmtools.sh
+++ applications/vmware/el7-install-openvmtools.sh
@@ -1,3 +1,4 @@
-#
+# You can run this script directly with the following command:
+# curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/applications/vmware/el7-install-openvmtools.sh | sudo bash
 
 sudo yum install -y open-vm-tools
