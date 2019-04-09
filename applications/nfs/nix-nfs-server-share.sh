111e7e97c629f24513d12d927a5667f1ed9275aa
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-04-09 13:30:53 -0500

    Update nix-nfs-server-share.sh

--- applications/nfs/nix-nfs-server-share.sh
+++ applications/nfs/nix-nfs-server-share.sh
@@ -1,6 +1,9 @@
-# You can run this script directly with the following command
 # Append your Client allowed IP Range followed by the NFS directory you want to create for example "| sudo bash -s 192.168.1.1 /var/nfsshare"
 # curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/applications/nfs/nix-nfs-server-share.sh | sudo bash -s 
+
+# https://www.howtoforge.com/nfs-server-and-client-on-centos-7
+# https://www.unixmen.com/setting-nfs-server-client-centos-7/
+# https://www.itzgeek.com/how-tos/linux/centos-how-tos/how-to-setup-nfs-server-on-centos-7-rhel-7-fedora-22.html
 
 # Set variable for you to specify directory
 NFS_CLIENT=$1
