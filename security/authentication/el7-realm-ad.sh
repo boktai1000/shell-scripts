81cef19e409c646fd78c6ff3663d682335cb413a
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-04-10 09:44:25 -0500

    Update el7-realm-ad.sh

--- security/authentication/el7-realm-ad.sh
+++ security/authentication/el7-realm-ad.sh
@@ -3,5 +3,5 @@
 
 yum install -y realmd oddjob oddjob-mkhomedir sssd samba-common-tools
 
-# ex: "realm join ad.example.com" - $1 is your domain and $2 is your administrator@ad.example.com password
+# ex: "realm join ad.example.com" - $1 is your domain and $2 is your Administrator@ad.example.com password
 echo $2 | realm join $1
