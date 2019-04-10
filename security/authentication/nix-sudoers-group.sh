44e048e794bf77dc77243a249a8574be8093fd60
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-04-10 14:36:07 -0500

    Update and rename nix-sudoers-group.sh to nix-configure-sudoers.sh

--- security/authentication/nix-sudoers-group.sh
+++
@@ -1,6 +0,0 @@
-# You can run this script directly with the following command
-# curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/security/authentication/nix-sudoers-group.sh | sudo bash -s 
-
-# https://www.reddit.com/r/sysadmin/comments/2dc7jf/best_way_to_automate_the_process_of_joining_linux/cjo65jf/
-# Add your group to Sudoers file - Note for Groups with spaces type as follows: linux\ administrators
-echo "%$1  ALL=(ALL) ALL" | sudo tee -a /etc/sudoers > /dev/null
