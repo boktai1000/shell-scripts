3cab2016c9dd86de6929e61f3b04955b94499bea
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-04-10 14:43:51 -0500

    Update el7-realm-ad-fqn.sh

--- security/authentication/el7-realm-ad-fqn.sh
+++ security/authentication/el7-realm-ad-fqn.sh
@@ -6,6 +6,7 @@
 # You can run this script directly with the following command, append domain name, workgroup, and then Admin password in that order
 # curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/security/authentication/el7-realm-ad-fqn.sh | sudo bash -s 
 
+# Install required packages for AD integration / join via realm
 yum install -y realmd oddjob oddjob-mkhomedir sssd samba-common-tools
 
 # ex: "realm join ad.example.com" - $1 is your domain and $2 is your Administrator@ad.example.com password
