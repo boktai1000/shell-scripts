016fdea0b9d3b0b0874b768ffefdb1da4e9e692a
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-04-17 16:17:39 -0500

    Update el7-install-vim.sh

--- applications/vim/el7-install-vim.sh
+++ applications/vim/el7-install-vim.sh
@@ -1,18 +1,18 @@
 # You can run this script directly with the following command
 # source <(curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/applications/vim/el7-install-vim.sh)
 
-# Install VIM - supersedes VI
+# Install VIM - supersedes VI and includes syntax highlighting
 sudo yum install -y vim
 
-# Backup vim.sh file
+# Backup /etc/profile.d/vim.sh
 cp /etc/profile.d/vim.sh /etc/profile.d/vim.sh.bak
 
-# Create new vim.sh that applies alias to all users instead of exclusing root and just checks for vim binary
+# Create new vim.sh that applies alias to all users if vim binary is present
 echo "if [ -n "$BASH_VERSION" -o -n "$KSH_VERSION" -o -n "$ZSH_VERSION" ];then
   if [ -f /usr/bin/vim ]; then
          alias vi='vim'
   fi
 fi" > /etc/profile.d/vim.sh
 
-# Reload profile without loading a new shell - so Alias updates properly/etc.
-source /etc/profile
+# Source vim.sh to load it
+. /etc/profile.d/vim.sh
