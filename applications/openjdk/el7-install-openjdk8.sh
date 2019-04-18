393f8e58eff92103c175b124f75bf7129482f478
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-04-18 14:33:12 -0500

    Update el7-install-openjdk8.sh

--- applications/openjdk/el7-install-openjdk8.sh
+++ applications/openjdk/el7-install-openjdk8.sh
@@ -1,4 +1,11 @@
+#!/bin/bash
+
 # https://openjdk.java.net/install/
 # https://www.digitalocean.com/community/tutorials/how-to-install-java-on-centos-and-fedora#install-openjdk-8
 
+# You can run this script directly with either of the following commands
+# curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/applications/openjdk/el7-install-openjdk8.sh | sudo bash
+# curl -s https://bitbucket.org/boktai1000/shell-scripts/raw/master/applications/openjdk/el7-install-openjdk8.sh | sudo bash
+
+# Install OpenJDK 8 from Repos
 sudo yum install java-1.8.0-openjdk -y
