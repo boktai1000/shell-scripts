1115afb0e0c8ce76b4b92d692492d3799e073288
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-04-18 14:54:30 -0500

    Update el7-install-openjdk11.sh

--- applications/openjdk/el7-install-openjdk11.sh
+++ applications/openjdk/el7-install-openjdk11.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# https://www.server-world.info/en/note?os=CentOS_7&p=jdk11&f=2
+# https://openjdk.java.net/install/
+# https://www.reddit.com/r/linuxquestions/comments/avja5n/reloading_current_bash_shell_from_script_thats/
+
 # You can run this script directly with either of the following commands
 # curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/applications/openjdk/el7-install-openjdk11.sh | sudo bash
 # curl -s https://bitbucket.org/boktai1000/shell-scripts/raw/master/applications/openjdk/el7-install-openjdk11.sh | sudo bash
