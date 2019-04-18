880a5f8f7a7face99b6d1e2fd946c646b4824aba
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-04-18 12:40:31 -0500

    Update ubuntu-install-cockpit.sh

--- applications/cockpit/ubuntu-install-cockpit.sh
+++ applications/cockpit/ubuntu-install-cockpit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# You can run this script directly with the following command
+# You can run this script directly with either of the following commands
 # curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/applications/cockpit/ubuntu-install-cockpit.sh | sudo bash
 # curl -s https://bitbucket.org/boktai1000/shell-scripts/raw/master/applications/cockpit/ubuntu-install-cockpit.sh | sudo bash
 
