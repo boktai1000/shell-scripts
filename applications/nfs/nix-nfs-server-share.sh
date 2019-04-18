351907748b58e608268689ee2f4f7dae974d0c49
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-04-18 13:12:20 -0500

    Update nix-nfs-server-share.sh

--- applications/nfs/nix-nfs-server-share.sh
+++ applications/nfs/nix-nfs-server-share.sh
@@ -1,20 +1,23 @@
-# Append your Client allowed IP Range followed by the NFS directory you want to create for example "| sudo bash -s 192.168.1.1 /var/nfsshare"
-# curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/applications/nfs/nix-nfs-server-share.sh | sudo bash -s 
+#!/bin/bash
 
 # https://www.howtoforge.com/nfs-server-and-client-on-centos-7
 # https://www.unixmen.com/setting-nfs-server-client-centos-7/
 # https://www.itzgeek.com/how-tos/linux/centos-how-tos/how-to-setup-nfs-server-on-centos-7-rhel-7-fedora-22.html
+
+# Append your Client allowed IP Range followed by the NFS directory you want to create for example "| sudo bash -s 192.168.1.1 /var/nfsshare"
+# curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/applications/nfs/nix-nfs-server-share.sh | sudo bash -s 
+# curl -s https://bitbucket.org/boktai1000/shell-scripts/raw/master/applications/nfs/nix-nfs-server-share.sh | sudo bash -s 
 
 # Set variable for you to specify directory
 NFS_CLIENT=$1
 NFS_SHARE=$2
 
 # Create directory
-mkdir $NFS_SHARE
+mkdir "$NFS_SHARE"
 
 # Set permissions on NFS directory
-chmod -R 755 $NFS_SHARE
-chown nfsnobody:nfsnobody $NFS_SHARE
+chmod -R 755 "$NFS_SHARE"
+chown nfsnobody:nfsnobody "$NFS_SHARE"
 
 # Add directory to exports for sharing
 echo "$NFS_SHARE    $NFS_CLIENT(rw,sync,no_root_squash,no_all_squash)" | sudo tee -a /etc/exports
