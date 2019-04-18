6fc26582b8d4d2c46ad176909b93217c2a8c4ed0
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-04-18 14:36:22 -0500

    Update tomcat-variable-test.sh

--- applications/tomcat/testing/tomcat-variable-test.sh
+++ applications/tomcat/testing/tomcat-variable-test.sh
@@ -1,10 +1,12 @@
-# You can run this script directly with the following command
+#!/bin/bash
+
 # Append desired version followed by desired port number - otherwise latest version and default port will be selected
 # curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/applications/tomcat/testing/tomcat-variable-test.sh | sudo bash -s 
+# curl -s https://bitbucket.org/boktai1000/shell-scripts/raw/master/applications/tomcat/testing/tomcat-variable-test.sh | sudo bash -s 
 
 tomcatlatest="$(curl -s https://api.github.com/repos/apache/tomcat/tags | grep '"name"' | head -1 | egrep -o "([0-9]{1,}\.)+[0-9]{1,}")"
 tomcatminorversion=${1:-$tomcatlatest}
-tomcatmajorversion="`echo $tomcatminorversion | cut -c1-1`"
+tomcatmajorversion="$(echo "$tomcatminorversion" | cut -c1-1)"
 yourip=$(hostname -I | awk '{print $1}')
 tomcatport="${2:-8080}"
 
@@ -17,7 +19,7 @@ echo "Your Minor Version $tomcatminorversion"
 echo "Your IP address $yourip"
 echo "Your Tomcat Port $tomcatport"
 echo "Tomcat Latest $tomcatlatest"
-echo "Proposed Tomcat download URL" https://archive.apache.org/dist/tomcat/tomcat-$tomcatmajorversion/v$tomcatminorversion/bin/apache-tomcat-$tomcatminorversion.tar.gz
+echo "Proposed Tomcat download URL" https://archive.apache.org/dist/tomcat/tomcat-"$tomcatmajorversion"/v"$tomcatminorversion"/bin/apache-tomcat-"$tomcatminorversion".tar.gz
 cat /tmp/8080testfile
 
 rm /tmp/8080testfile
