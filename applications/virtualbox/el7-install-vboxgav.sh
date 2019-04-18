a728cdbe95be2905e29c68e1ad075feb324bbe44
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-04-18 13:00:01 -0500

    Update el7-install-vboxgav.sh

--- applications/virtualbox/el7-install-vboxgav.sh
+++ applications/virtualbox/el7-install-vboxgav.sh
@@ -7,18 +7,17 @@
 # Set VirtualBox Variable so argument from command line gets passed through
 # https://gist.github.com/jonasschultzmblox/f15fe3c10769d5f269635a54394c84d4#gistcomment-2023609
 VBOX_VERSION=$1
-ARCH=`uname -r |cut -f7 -d.`
-KVER=`uname -r |cut -f1-6 -d.`
+ARCH=$(uname -r |cut -f7 -d.)
+KVER=$(uname -r |cut -f1-6 -d.)
 
 # Install prereqs - install matching kernel tools and headers
 yum install -y bzip2 gcc kernel-devel-"${KVER}"."${ARCH}"
 
-# Change to temp directory and download VboxGuestAdditions
-cd /tmp || exit
-curl -O http://download.virtualbox.org/virtualbox/"$VBOX_VERSION"/VBoxGuestAdditions_"$VBOX_VERSION".iso
+# Download VboxGuestAdditions
+(cd /tmp && curl -O http://download.virtualbox.org/virtualbox/"$VBOX_VERSION"/VBoxGuestAdditions_"$VBOX_VERSION".iso)
 
 # Mount and run iso
-mount -o loop,ro VBoxGuestAdditions_"$VBOX_VERSION".iso /mnt
+mount -o loop,ro /tmp/VBoxGuestAdditions_"$VBOX_VERSION".iso /mnt
 sh /mnt/VBoxLinuxAdditions.run
 
 # Cleanup
