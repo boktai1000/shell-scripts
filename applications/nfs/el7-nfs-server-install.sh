79de18d426026210e9294387b1719819d840ac23
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-04-18 13:09:24 -0500

    Update el7-nfs-server-install.sh

--- applications/nfs/el7-nfs-server-install.sh
+++ applications/nfs/el7-nfs-server-install.sh
@@ -1,5 +1,8 @@
-# You can run this script directly with the following command
+#!/bin/bash
+
+# You can run this script directly with either of the following commands
 # curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/applications/nfs/el7-nfs-server-install.sh | sudo bash
+# curl -s https://bitbucket.org/boktai1000/shell-scripts/raw/master/applications/nfs/el7-nfs-server-install.sh | sudo bash
 
 # Install NFS Packages
 yum install -y nfs-utils
