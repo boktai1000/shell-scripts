dfb7743ab56c80ec609d51be96be19da1858555a
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-04-18 19:35:45 -0500

    Update nix-install-cockpit.sh

--- applications/cockpit/nix-install-cockpit.sh
+++ applications/cockpit/nix-install-cockpit.sh
@@ -7,36 +7,34 @@
 # curl -s https://bitbucket.org/boktai1000/shell-scripts/raw/master/applications/cockpit/nix-install-cockpit.sh | sudo bash
 
 if [ "$EUID" -ne "0" ]; then
-  echo "This script must be run as root." >&2
-  exit 1
+    echo "This script must be run as root." >&2
+    exit 1
 fi
 
-  # Set Variable for your IP Address
-  yourip=$(hostname -I | awk '{print $1}')
+# Set Variable for your IP Address
+yourip=$(hostname -I | awk '{print $1}')
 
 if [ -f /etc/redhat-release ]; then
+    
+    # Install cockpit
+    yum -y install cockpit setroubleshoot-server sos
+    
+    # Enable cockpit
 
-  # Install cockpit
-  yum -y install cockpit setroubleshoot-server sos
+    systemctl enable --now cockpit.socket
 
-  # Enable cockpit
-  systemctl enable --now cockpit.socket
+    # Configure firewall
+    firewall-cmd --permanent --zone=public --add-service=cockpit
+    firewall-cmd --reload
 
-  # Configure firewall
-  firewall-cmd --permanent --zone=public --add-service=cockpit
-  firewall-cmd --reload
-
-  # Echo a reminder to CLI on how to login to Cockpit
-  echo Login to Cockpit at https://"$yourip":9090
-
+    # Echo a reminder to CLI on how to login to Cockpit
+    echo Login to Cockpit at https://"$yourip":9090
 fi
-
 if [ -f /etc/lsb-release ]; then
-
-  # Install Cockpit
-  sudo apt-get install -y cockpit
-
-  # Echo a reminder to CLI on how to login to Cockpit
-  echo Login to Cockpit at https://"$yourip":9090
-
+    
+    # Install Cockpit
+    sudo apt-get install -y cockpit
+    
+    # Echo a reminder to CLI on how to login to Cockpit
+    echo Login to Cockpit at https://"$yourip":9090
 fi
