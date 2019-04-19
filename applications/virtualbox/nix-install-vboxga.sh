2fb946dada5f56bddabac0fc24c4413232520004
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-04-19 16:20:46 -0500

    Update nix-install-vboxga.sh

--- applications/virtualbox/nix-install-vboxga.sh
+++ applications/virtualbox/nix-install-vboxga.sh
@@ -2,6 +2,8 @@
 
 # https://gist.github.com/jonasschultzmblox/f15fe3c10769d5f269635a54394c84d4#gistcomment-2023609
 # https://gist.github.com/dirtmerchant/a32007dc2980bbccda3de0873e28340e
+# https://www.tecmint.com/install-virtualbox-guest-additions-in-ubuntu/
+# https://askubuntu.com/questions/22743/how-do-i-install-guest-additions-in-a-virtualbox-vm
 
 # Append your version number after "| sudo bash -s" ; Example- "| sudo bash -s 6.0.4"
 # curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/applications/virtualbox/nix-install-vboxga.sh | sudo bash -s
