a75ee8211198461b12b4b55f20e7bcb78ab7103e
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-04-19 23:13:29 -0500

    Update el7-update-unifi.sh

--- applications/unifi/el7-update-unifi.sh
+++ applications/unifi/el7-update-unifi.sh
@@ -1,6 +1,8 @@
-#Based on https://community.ubnt.com/t5/UniFi-Wireless/Installing-UniFi-on-CentOS7-as-a-service/m-p/1973439/highlight/true#M234790
+#!/bin/bash
 
-#You can run this script directly with the following command
+# Based on https://community.ubnt.com/t5/UniFi-Wireless/Installing-UniFi-on-CentOS7-as-a-service/m-p/1973439/highlight/true#M234790
+
+# You can run this script directly with the following command
 # curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/applications/unifi/el7-update-unifi.sh | sudo bash
 
 #Set Version (LTS Stable)
@@ -10,8 +12,7 @@ unifiversion="$(curl -s https://help.ubnt.com/hc/en-us/articles/360008240754#1 |
 systemctl stop unifi
 
 #Download latest software:
-cd /usr/src
-curl -O http://dl.ubnt.com/unifi/$unifiversion/UniFi.unix.zip
+(cd /tmp && curl -O http://dl.ubnt.com/unifi/"$unifiversion"/UniFi.unix.zip)
 
 #Extract UniFi Video software:
 unzip UniFi.unix.zip -d /opt/
