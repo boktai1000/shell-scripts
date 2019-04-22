a64158eaa95a32f9adb35b5d77655b079f7d9db5
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-04-22 16:17:08 -0500

    Update and rename openssh-old-user.sh to openssh-old-adduser.sh

--- security/openssh/openssh-old-adduser.sh
+++ security/openssh/openssh-old-adduser.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # You can run this script directly with the following command followed by your desired username and then password
-# curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/security/openssh/openssh-old-user.sh | sudo bash
+# curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/security/openssh/openssh-old-adduser.sh | sudo bash
 
 # https://community.cisco.com/t5/unified-communications/cucm-sftp-backup-to-linux-fail/td-p/2805926
 # http://cdesigner.eu/content/14-cucm-8-free-sftp-solution-backup-ubuntu-1004-server
