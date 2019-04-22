e906572d6177d2b782be24172d9f4d34dee253d8
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-04-22 14:23:07 -0500

    Update nix-install-openjdk12.sh

--- applications/openjdk/nix-install-openjdk12.sh
+++ applications/openjdk/nix-install-openjdk12.sh
@@ -9,17 +9,20 @@
 # source <(curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/applications/openjdk/nix-install-openjdk12.sh)
 # source <(curl -s https://bitbucket.org/boktai1000/shell-scripts/raw/master/applications/openjdk/nix-install-openjdk12.sh)
 
+# Set variables - Optionally specify install directory or default to /opt
+JAVA_DIR="${1-/opt}"
+
 # Download OpenJDK
 (cd /tmp && curl -O https://download.java.net/java/GA/jdk12.0.1/69cfe15208a647278a19ef0990eea691/12/GPL/openjdk-12.0.1_linux-x64_bin.tar.gz)
 
 # Un-tar OpenJDK to installation location
-tar zxvf /tmp/openjdk-12.0.1_linux-x64_bin.tar.gz -C /opt/
+tar zxvf /tmp/openjdk-12.0.1_linux-x64_bin.tar.gz -C "$JAVA_DIR"/
 
 # Cleanup downloaded tar file
 rm -f /tmp/openjdk-12.0.1_linux-x64_bin.tar.gz
 
 # Create jdk.sh file that will set variables when users login to system
-echo export JAVA_HOME=/opt/jdk-12.0.1 | sudo tee /etc/profile.d/jdk12.sh
+echo export JAVA_HOME="$JAVA_DIR"/jdk-12.0.1 | sudo tee /etc/profile.d/jdk12.sh
 echo 'export PATH=$PATH:$JAVA_HOME/bin' | sudo tee -a /etc/profile.d/jdk12.sh
 
 # Load the script now so variables are set
