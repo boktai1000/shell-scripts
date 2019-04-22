bc85ce4337c750d3b87156d24810ae0585098f97
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-04-22 11:30:26 -0500

    Update nix-install-openjdk12.sh

--- applications/openjdk/nix-install-openjdk12.sh
+++ applications/openjdk/nix-install-openjdk12.sh
@@ -9,21 +9,21 @@
 # source <(curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/applications/openjdk/nix-install-openjdk12.sh)
 # source <(curl -s https://bitbucket.org/boktai1000/shell-scripts/raw/master/applications/openjdk/nix-install-openjdk12.sh)
 
-# Download OpenJDK 11
+# Download OpenJDK
 (cd /tmp && curl -O https://download.java.net/java/GA/jdk12.0.1/69cfe15208a647278a19ef0990eea691/12/GPL/openjdk-12.0.1_linux-x64_bin.tar.gz)
 
-# Un-tar OpenJDK 11 to installation location
+# Un-tar OpenJDK to installation location
 tar zxvf /tmp/openjdk-12.0.1_linux-x64_bin.tar.gz -C /opt/
 
 # Cleanup downloaded tar file
 rm -f /tmp/openjdk-12.0.1_linux-x64_bin.tar.gz
 
-# Create jdk11.sh file that will set variables when users login to system
-echo export JAVA_HOME=/opt/jdk-11.0.2 | sudo tee /etc/profile.d/jdk12.sh
+# Create jdk.sh file that will set variables when users login to system
+echo export JAVA_HOME=/opt/jdk-12.0.1 | sudo tee /etc/profile.d/jdk12.sh
 echo 'export PATH=$PATH:$JAVA_HOME/bin' | sudo tee -a /etc/profile.d/jdk12.sh
 
 # Load the script now so variables are set
-. /etc/profile.d/jdk11.sh
+. /etc/profile.d/jdk12.sh
 
 # Validate and show current Java version
 java -version
