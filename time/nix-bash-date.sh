4e1a4dc7feffdf20320039018d4c5ccf48b81e9c
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-04-23 14:47:19 -0500

    Update nix-bash-date.sh

--- time/nix-bash-date.sh
+++ time/nix-bash-date.sh
@@ -3,8 +3,8 @@
 # https://zxq9.com/archives/795
 
 # You can run this script directly with either of the following commands
-# curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/time/bash-date.sh | sudo bash
-# curl -s https://bitbucket.org/boktai1000/shell-scripts/raw/master/time/bash-date.sh | sudo bash
+# curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/time/nix-bash-date.sh | sudo bash
+# curl -s https://bitbucket.org/boktai1000/shell-scripts/raw/master/time/nix-bash-date.sh | sudo bash
 
 cat << EOD
         Format/result           |       Command              |          Output
