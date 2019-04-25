d6e6c2ae0f200632300dbd188cf0a1d2f8e917b3
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-04-24 21:35:38 -0500

    Update nix-install-tomcat9.sh

--- applications/tomcat/nix-install-tomcat9.sh
+++ applications/tomcat/nix-install-tomcat9.sh
@@ -30,7 +30,7 @@ After=network.target
 [Service]
 Type=forking
 
-Environment=JAVA_HOME=/usr/local/jdk-11.0.2
+Environment=JAVA_HOME=$JAVA_HOME
 Environment=CATALINA_PID=/opt/tomcat/temp/tomcat.pid
 Environment=CATALINA_HOME=/opt/tomcat
 Environment=CATALINA_BASE=/opt/tomcat
