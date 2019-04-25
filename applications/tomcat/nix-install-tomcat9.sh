05660a3359f0f633877e0c237da449638402e9c5
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-04-25 00:03:26 -0500

    Update nix-install-tomcat9.sh

--- applications/tomcat/nix-install-tomcat9.sh
+++ applications/tomcat/nix-install-tomcat9.sh
@@ -12,9 +12,7 @@ groupadd tomcat
 useradd -g tomcat -d /opt/tomcat -s /bin/nologin tomcat
 
 (cd /tmp && curl -O https://www-us.apache.org/dist/tomcat/tomcat-9/v"$tomcatversion"/bin/apache-tomcat-"$tomcatversion".tar.gz)
-
 tar -xzvf /tmp/apache-tomcat-"$tomcatversion".tar.gz -C /opt/tomcat --strip-components=1
-
 (cd /opt/tomcat/webapps/ && sudo rm -rf docs examples manager host-manager)
 
 chown -R tomcat:tomcat /opt/tomcat/
