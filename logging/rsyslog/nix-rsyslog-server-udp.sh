51fc4be983950458789f2b7edb09d968cbcc491f
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-04-26 13:55:09 -0500

    Create nix-rsyslog-server-udp.sh

---
+++ logging/rsyslog/nix-rsyslog-server-udp.sh
@@ -0,0 +1,15 @@
+# You can run this script directly with the following command
+# curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/logging/rsyslog/nix-rsyslog-server-udp.sh | sudo bash
+
+# Backup /etc/rsyslog.conf
+cp /etc/rsyslog.conf /etc/rsyslog.conf.bak
+
+# Configure Rsyslog to accept remote log messages using UDP port 514
+sed -i 's/#$ModLoad imudp/$ModLoad imudp/g' /etc/rsyslog.conf
+sed -i 's/#$UDPServerRun 514/$UDPServerRun 514/g' /etc/rsyslog.conf
+
+systemctl restart rsyslog
+
+# Open firewall to receive remote log messages using UDP port 514
+firewall-cmd --permanent --add-port=514/udp
+firewall-cmd –reload
