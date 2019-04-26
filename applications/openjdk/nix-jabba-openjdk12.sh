6e2d69b4c25cb5d4bfafc877dced9d967fb7ea6f
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-04-26 18:32:53 -0500

    Update nix-jabba-openjdk12.sh

--- applications/openjdk/nix-jabba-openjdk12.sh
+++ applications/openjdk/nix-jabba-openjdk12.sh
@@ -5,8 +5,8 @@
 # https://jdk.java.net/
 
 # You can run this script directly with either of the following commands
-# source <(curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/applications/openjdk/nix-jabba-openjdk12.sh.sh)
-# source <(curl -s https://bitbucket.org/boktai1000/shell-scripts/raw/master/applications/openjdk/nix-jabba-openjdk12.sh.sh)
+# source <(curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/applications/openjdk/nix-jabba-openjdk12.sh)
+# source <(curl -s https://bitbucket.org/boktai1000/shell-scripts/raw/master/applications/openjdk/nix-jabba-openjdk12.sh)
 
 # Install Jabba
 curl -sL https://github.com/shyiko/jabba/raw/master/install.sh | bash && . ~/.jabba/jabba.sh
