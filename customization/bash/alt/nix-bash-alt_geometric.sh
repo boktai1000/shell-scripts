e3d123dd2a832398f7cc1ab12cc559e5b1dcd71c
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-04-27 14:23:12 -0500

    Update nix-bash-alt_geometric.sh

--- customization/bash/alt/nix-bash-alt_geometric.sh
+++ customization/bash/alt/nix-bash-alt_geometric.sh
@@ -13,3 +13,6 @@ else
     PS1="\[\033[m\]|\[\033[1;35m\]\t\[\033[m\]|\[\e[1m\]\u\[\e[1;36m\]\[\033[m\]@\[\e[1;36m\]\h\[\033[m\]:\[\e[0m\]\[\e[1;32m\][\W]> \[\e[0m\]"
 fi
 EOF
+
+# Load new bash settings
+. /etc/profile.d/fancy-bash-prompt.sh
