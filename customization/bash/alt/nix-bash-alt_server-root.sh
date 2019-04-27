e3781d058fbcd27c397976b6f2715ee1ee7c9e0a
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-04-27 14:16:23 -0500

    Update nix-bash-alt_server-root.sh

--- customization/bash/alt/nix-bash-alt_server-root.sh
+++ customization/bash/alt/nix-bash-alt_server-root.sh
@@ -2,14 +2,14 @@
 # You can run this script directly with the following command
 # source <(curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/customization/bash/alt/nix-bash-alt_server-root.sh)
 
-# https://askubuntu.com/a/549150 
+# https://askubuntu.com/a/549150
 # https://www.reddit.com/r/linux/comments/2uf5uu/this_is_my_bash_prompt_which_is_your_favorite/?depth=2
 sudo tee /etc/profile.d/fancy-bash-prompt.sh <<\EOF
 #!/bin/bash
 
 if [[ "${EUID}" -eq 0 ]]; then
-    export PS1="\[\e[01;37m\][\[\e[0m\]\[\e[01;32m\]\u\[\e[0m\]\[\e[00;37m\]@\[\e[0m\]\[\e[01;34m\]\h\[\e[0m\]\[\e[00;37m\] \[\e[0m\]\[\e[00;37m\]\t\[\e[0m\]\[\e[01;37m\] \W]\\$ \[\e[0m\]"
-else
     export PS1="\[\e[01;37m\][\[\e[0m\]\[\e[01;31m\]\u\[\e[0m\]\[\e[00;37m\]@\[\e[0m\]\[\e[01;34m\]\h\[\e[0m\]\[\e[00;37m\] \[\e[0m\]\[\e[00;37m\]\t\[\e[0m\]\[\e[01;37m\] \W]\\$ \[\e[0m\]"
+else
+    export PS1="\[\e[01;37m\][\[\e[0m\]\[\e[01;32m\]\u\[\e[0m\]\[\e[00;37m\]@\[\e[0m\]\[\e[01;34m\]\h\[\e[0m\]\[\e[00;37m\] \[\e[0m\]\[\e[00;37m\]\t\[\e[0m\]\[\e[01;37m\] \W]\\$ \[\e[0m\]"
 fi
 EOF
