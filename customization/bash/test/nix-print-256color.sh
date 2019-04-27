c7654eb6bc523563245c0f061a1dcf6279937624
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-04-27 12:42:45 -0500

    Create nix-print-256color.sh

---
+++ customization/bash/test/nix-print-256color.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# https://askubuntu.com/questions/821157/print-a-256-color-test-pattern-in-the-terminal 
+
+curl -s https://gist.githubusercontent.com/HaleTom/89ffe32783f89f403bba96bd7bcd1263/raw/ | bash
