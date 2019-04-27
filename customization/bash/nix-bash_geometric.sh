545062603c702f6fb8295015c4560f7833abce55
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-04-27 14:21:41 -0500

    Create nix-bash_geometric.sh

---
+++ customization/bash/nix-bash_geometric.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# You can run this script directly with the following command
+# source <(curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/customization/bash/nix-bash_geometric.sh)
+
+# https://askubuntu.com/a/549150
+# https://www.reddit.com/r/linux/comments/2uf5uu/this_is_my_bash_prompt_which_is_your_favorite/?depth=2
+sudo bash -c 'cat > /etc/profile.d/fancy-bash-prompt.sh' <<\EOF
+#!/bin/bash
+
+if [[ "${EUID}" -eq 0 ]]; then
+    PS1="\[\033[m\]|\[\033[1;35m\]\t\[\033[m\]|\[\e[1;31m\]\u\[\e[1;36m\]\[\033[m\]@\[\e[1;36m\]\h\[\033[m\]:\[\e[0m\]\[\e[1;32m\][\W]> \[\e[0m\]"
+else
+    PS1="\[\033[m\]|\[\033[1;35m\]\t\[\033[m\]|\[\e[1m\]\u\[\e[1;36m\]\[\033[m\]@\[\e[1;36m\]\h\[\033[m\]:\[\e[0m\]\[\e[1;32m\][\W]> \[\e[0m\]"
+fi
+EOF
