64443a45e6965d0a91e1d6bb8e81ef24c466232c
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-04-27 10:25:12 -0500

    Update red-white_shell.sh

--- customization/bash/red-white_shell.sh
+++ customization/bash/red-white_shell.sh
@@ -3,7 +3,7 @@
 
 # Color via https://askubuntu.com/a/549150 (Server root)
 sudo bash -c 'cat > /etc/profile.d/red-white_shell.sh' << EOF
-export PS1="\[\e[01;37m\][\[\e[0m\]\[\e[01;31m\]\u\[\e[0m\]\[\e[00;37m\]@\[\e[0m\]\[\e[01;34m\]\h\[\e[0m\]\[\e[00;37m\] \[\e[0m\]\[\e[00;37m\]\t\[\e[0m\]\[\e[01;37m\] \W]\$ \[\e[0m\]"
+export PS1="\[\e[01;37m\][\[\e[0m\]\[\e[01;31m\]\u\[\e[0m\]\[\e[00;37m\]@\[\e[0m\]\[\e[01;34m\]\h\[\e[0m\]\[\e[00;37m\] \[\e[0m\]\[\e[00;37m\]\t\[\e[0m\]\[\e[01;37m\] \W]\\$ \[\e[0m\]"
 EOF
 
 # Load new bash settings
