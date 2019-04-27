6dcf1c32105f42bec9e6641c02a6536a4a81feb1
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-04-27 10:27:16 -0500

    Update red-white_shell.sh

--- customization/bash/red-white_shell.sh
+++ customization/bash/red-white_shell.sh
@@ -2,6 +2,7 @@
 # source <(curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/customization/bash/red-white_shell.sh)
 
 # Color via https://askubuntu.com/a/549150 (Server root)
+# Don't truncate otherwise the console will have a light blue / flashing problem
 sudo bash -c 'cat > /etc/profile.d/red-white_shell.sh' << EOF
 export PS1="\[\e[01;37m\][\[\e[0m\]\[\e[01;31m\]\u\[\e[0m\]\[\e[00;37m\]@\[\e[0m\]\[\e[01;34m\]\h\[\e[0m\]\[\e[00;37m\] \[\e[0m\]\[\e[00;37m\]\t\[\e[0m\]\[\e[01;37m\] \W]\\$ \[\e[0m\]"
 EOF
