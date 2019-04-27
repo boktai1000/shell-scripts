22b753d1def735cd34fd1080a95f04034602c8e4
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-04-27 15:44:03 -0500

    Update nix-bash_server-root.sh

--- customization/bash/nix-bash_server-root.sh
+++ customization/bash/nix-bash_server-root.sh
@@ -7,8 +7,6 @@
 # https://serverfault.com/a/506267/480927
 # https://www.reddit.com/r/linux/comments/2uf5uu/this_is_my_bash_prompt_which_is_your_favorite/?depth=2
 sudo bash -c 'cat > /etc/profile.d/fancy-bash-prompt.sh' <<\EOF
-#!/bin/bash
-
 if [[ "${EUID}" -eq 0 ]]; then
     export PS1="\[\e[01;37m\][\[\e[0m\]\[\e[01;31m\]\u\[\e[0m\]\[\e[00;37m\]@\[\e[0m\]\[\e[01;34m\]\h\[\e[0m\]\[\e[00;37m\] \[\e[0m\]\[\e[00;37m\]\t\[\e[0m\]\[\e[01;37m\] \W]\\$ \[\e[0m\]"
 else
