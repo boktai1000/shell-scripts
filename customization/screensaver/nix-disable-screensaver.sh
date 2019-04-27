ed68e569389bb6b4700301cbb27117b7d748fe37
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-04-27 10:07:18 -0500

    Update nix-disable-screensaver.sh

--- customization/screensaver/nix-disable-screensaver.sh
+++ customization/screensaver/nix-disable-screensaver.sh
@@ -3,6 +3,10 @@
 # Source
 # https://unix.stackexchange.com/a/425719 
 
+# You can run this script directly with the following command
+# curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/customization/screensaver/nix-disable-screensaver.sh | sudo bash
+# curl -s https://bitbucket.org/boktai1000/shell-scripts/raw/master/customization/screensaver/nix-disable-screensaver.sh | sudo bash
+
 # Disable Console screensaver
 TERM=linux setterm -blank 0 -powerdown 0  -powersave off >/dev/tty0 </dev/tty0
 
