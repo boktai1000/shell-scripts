cd1967d015c6f4ce74c7ca2104221a7966eeb824
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-05-02 12:03:34 -0500

    Update nix-install-tomcat9.sh

--- applications/tomcat/nix-install-tomcat9.sh
+++ applications/tomcat/nix-install-tomcat9.sh
@@ -4,7 +4,7 @@
 # curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/applications/tomcat/nix-install-tomcat9.sh | sudo bash
 
 # Set Variable to always download latest version of Tomcat 9 - Scrape Web Page for Version Number
-tomcatversion="$(curl -s https://www-us.apache.org/dist/tomcat/tomcat-9/ | grep -Po '(?<=(<a href="v)).*(?=/">v)' | sort -r | head -1)"
+tomcatversion="$(curl -s https://www-us.apache.org/dist/tomcat/tomcat-9/ | grep -Po '(?<=(<a href="v)).*(?=/">v)' | sort -rV | head -1)"
 yourip=$(hostname -I | awk '{print $1}')
 jdkenv="$(cat /etc/profile.d/jdk*.sh | sed -nr '/JAVA_HOME=/ s/.*JAVA_HOME=([^"]+).*/\1/p' | head -1)"
 
