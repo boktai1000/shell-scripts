fc6825619b218be57dbff04116e5f31411fff932
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-05-02 12:40:12 -0500

    Update nix-tomcat-rsyslog-tcp.sh

--- logging/tomcat/nix-tomcat-rsyslog-tcp.sh
+++ logging/tomcat/nix-tomcat-rsyslog-tcp.sh
@@ -3,28 +3,31 @@
 # You can run this script directly with the following command
 # curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/logging/tomcat/nix-tomcat-rsyslog-tcp.sh | sudo bash
 
+elastic=${1:-x.x.x.x}
+
 # Configuring the tomcat.conf file
-echo "# File 1
+sudo tee /etc/rsyslog.d/tomcat.conf <<EOF
+# File 1
 input(type="imfile"
       File="/opt/tomcat*/logs/*"
       Tag="catalina"
       StateFile="/var/spool/catalina"
       Severity="info"
       Facility="local1")
-
-local1.* @@x.x.x.x:514" > /etc/rsyslog.d/tomcat.conf
+local1.* @@$elastic:514"
+EOF
 
 # Backup /etc/rsyslog.conf
 cp /etc/rsyslog.conf /etc/rsyslog.conf.bak-"$(date --utc +%FT%T.%3NZ)"
 
 # Configuring the rsyslog.conf - load the imfile module
-sed '22a\module(load="imfile" PollingInterval="10")' /etc/rsyslog.conf > /etc/rsyslog.conf
+sed -i '22a\module(load="imfile" PollingInterval="10")' /etc/rsyslog.conf
 
 # Configuring the rsyslog.conf - Configure the messages
 sed -i 's/cron.none/cron.none;local1.none/g' /etc/rsyslog.conf
 
 # Configure rsyslog to send rsyslog events to another server using TCP
-sed -i 's/# ### end of the forwarding rule ###/*.* @@$x.x.x.x:514/g' /etc/rsyslog.conf
+sed -i "s/# ### end of the forwarding rule ###/*.* @@$elastic:514/g" /etc/rsyslog.conf
 echo "# ### end of the forwarding rule ###" >> /etc/rsyslog.conf
 
 # Configuring the rsyslog.conf - Restart the rsyslog daemon
