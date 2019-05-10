8fb85670d23759eee93ea27aa39c5649238fe6cc
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-05-09 19:32:31 -0500

    Update nix-install-tomcat7.sh

--- applications/tomcat/nix-install-tomcat7.sh
+++ applications/tomcat/nix-install-tomcat7.sh
@@ -17,7 +17,7 @@ groupadd tomcat
 useradd -g tomcat -d /opt/tomcat -s /bin/nologin tomcat
 
 (cd /tmp && curl -O https://archive.apache.org/dist/tomcat/tomcat-7/v"$tomcatversion"/bin/apache-tomcat-"$tomcatversion".tar.gz)
-tar xzf /tmp/apache-tomcat-"$tomcatversion".tar.gz -C /usr/local/tomcat7 --strip-components=1
+mkdir -p /opt/tomcat && tar xzf /tmp/apache-tomcat-"$tomcatversion".tar.gz -C /usr/local/tomcat7 --strip-components=1
 (cd /usr/local/tomcat7/webapps/ && sudo rm -rf docs examples manager host-manager)
 
 chown -R tomcat:tomcat /usr/local/tomcat7
