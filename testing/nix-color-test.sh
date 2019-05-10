f932f9dad6b7efb141519c271d46649269422acd
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-05-09 23:25:20 -0500

    Update and rename nix-print-256color.sh to nix-color-test.sh

--- testing/nix-color-test.sh
+++ testing/nix-color-test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # You can run this script directly with the following command
-# curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/customization/testing/nix-print-256color.sh | sudo bash
+# curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/customization/testing/nix-color-test.sh | sudo bash
 
 # https://askubuntu.com/questions/821157/print-a-256-color-test-pattern-in-the-terminal 
 curl -s https://gist.githubusercontent.com/HaleTom/89ffe32783f89f403bba96bd7bcd1263/raw/ | bash
