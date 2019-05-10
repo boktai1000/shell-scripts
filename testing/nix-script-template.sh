6cbc6339793bc3642ec9fc28123c9edf8cf9312f
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-05-09 23:33:01 -0500

    Update nix-script-template.sh

--- testing/nix-script-template.sh
+++ testing/nix-script-template.sh
@@ -3,3 +3,11 @@
 # You can run this script directly with either of the following commands
 # curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/ | sudo bash
 # curl -s https://bitbucket.org/boktai1000/shell-scripts/raw/master/ | sudo bash
+
+# Set Variable for your IP Address
+yourip=$(hostname -I | awk '{print $1}')
+
+if [ "$EUID" -ne "0" ]; then
+    echo "This script must be run as root." >&2
+    exit 1
+fi
