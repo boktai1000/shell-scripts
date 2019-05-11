e04f0ae96f326572d4341c6cccc43ac486b5f781
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-05-11 10:54:41 -0500

    Update nix-rsyslog-server-udp.sh

--- logging/rsyslog/nix-rsyslog-server-udp.sh
+++ logging/rsyslog/nix-rsyslog-server-udp.sh
@@ -18,25 +18,62 @@ if [ -f /etc/redhat-release ]; then
     
     systemctl restart rsyslog
     
-    # Open Firewall for Rsyslog
-    echo 'Opening Firewall ports 514 TCP and UDP for Rsyslog'
+    # Opening Firewall port 514 UDP for Rsyslog
+    echo 'Opening Firewall port 514 UDP for Rsyslog'
     firewall-cmd --add-port=514/udp > /dev/null
     firewall-cmd --permanent --add-port=514/udp > /dev/null
     
 fi
 
 # Debian like specific commands and variables - only tested on Ubuntu
-if [ -f /etc/lsb-release ]; then
+if [ -f /etc/debian_version ]; then
+    debianID=$(lsb_release -is)
+    DIST="$(lsb_release -cs)"
     
-    # Configure Rsyslog to accept remote log messages using UDP port 514
-    sed -i 's/#module(load="imudp")/module(load="imudp")/g' /etc/rsyslog.conf
-    sed -i 's/#input(type="imudp" port="514")/input(type="imudp" port="514")/g' /etc/rsyslog.conf
+    # Ubuntu
+    if [ "${debianID}" = "Ubuntu" ]; then
+        
+        # Configure Rsyslog to accept remote log messages using UDP port 514
+        sed -i 's/#module(load="imudp")/module(load="imudp")/g' /etc/rsyslog.conf
+        sed -i 's/#input(type="imudp" port="514")/input(type="imudp" port="514")/g' /etc/rsyslog.conf
+        
+        systemctl restart rsyslog
+        
+        # Opening Firewall port 514 UDP for Rsyslog
+        echo 'Opening Firewall port 514 UDP for Rsyslog'
+        ufw allow 514/udp > /dev/null
+        
+        ufw reload
+        
+    fi
     
-    systemctl restart rsyslog
+    # Debian 9 (stretch)
+    if [ "${DIST}" = "stretch" ]; then
+        
+        # Configure Rsyslog to accept remote log messages using UDP port 514
+        sed -i 's/#module(load="imudp")/module(load="imudp")/g' /etc/rsyslog.conf
+        sed -i 's/#input(type="imudp" port="514")/input(type="imudp" port="514")/g' /etc/rsyslog.conf
+        
+        systemctl restart rsyslog
+        
+        # Opening Firewall port 514 UDP for Rsyslog
+        echo 'Opening Firewall port 514 UDP for Rsyslog'
+        iptables -I INPUT -m state --state NEW -m udp -p udp --dport 514 -j ACCEPT
+        
+    fi
     
-    # Open Firewall for Rsyslog
-    echo 'Opening Firewall port 514 UDP for Rsyslog'
-    ufw allow 514/udp > /dev/null
-    ufw reload
-    
+    # Debian 8 (Jessie)
+    if [ "${DIST}" = "jessie" ]; then
+        
+        # Configure Rsyslog to accept remote log messages using UDP port 514
+        sed -i 's/#$ModLoad imudp/$ModLoad imudp/g' /etc/rsyslog.conf
+        sed -i 's/#$UDPServerRun 514/$UDPServerRun 514/g' /etc/rsyslog.conf
+        
+        systemctl restart rsyslog
+        
+        # Opening Firewall port 514 UDP for Rsyslog
+        echo 'Opening Firewall port 514 UDP for Rsyslog'
+        iptables -I INPUT -m state --state NEW -m udp -p udp --dport 514 -j ACCEPT
+        
+    fi
 fi
