2d868f350a6a7511739ecfa450241853217d214f
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-06-13 10:17:44 -0500

    Update nix-install-terraform.sh

--- applications/terraform/nix-install-terraform.sh
+++ applications/terraform/nix-install-terraform.sh
@@ -9,17 +9,17 @@
 
 # Set variables (Note: default version of 0.11.11)
 TERRAFORM_VERSION=${1:-0.11.11}
-TERRAFORM_FILENAME=terraform_$TERRAFORM_VERSION_linux_amd64.zip
+TERRAFORM_FILENAME=terraform_"$TERRAFORM_VERSION"_linux_amd64.zip
 TERRAFORM_DOWNLOAD_ADDRESS=https://releases.hashicorp.com/terraform/$TERRAFORM_VERSION/$TERRAFORM_FILENAME
 
 # Install Unzip
 sudo apt-get install unzip -y
 
 # Download latest version of the terraform
-(cd /tmp && curl -O $TERRAFORM_DOWNLOAD_ADDRESS)
+(cd /tmp && curl -O "$TERRAFORM_DOWNLOAD_ADDRESS")
 
 # Extract the downloaded file archive
-unzip /tmp/$TERRAFORM_FILENAME
+unzip /tmp/"$TERRAFORM_FILENAME"
 
 # Move the executable into a directory searched for executables
 sudo mv /tmp/terraform /usr/local/bin/
