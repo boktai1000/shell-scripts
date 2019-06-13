14fb3c3ae2dd4582ed149c42d21ac971fcbae6b7
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-06-13 15:14:16 -0500

    Create nix-install-mstoolset.sh

---
+++ applications/microsoft/nix-install-mstoolset.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+# You can run this script directly with either of the following commands
+# curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/applications/microsoft/nix-install-mstoolset.sh | sudo bash
+# curl -s https://bitbucket.org/boktai1000/shell-scripts/raw/master/applications/microsoft/nix-install-mstoolset.sh | sudo bash
+
+
+# Check for Red Hat-based distro
+if [ -f /etc/redhat-release ]; then
+    
+    # Install Azure CLI
+    sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+    sudo sh -c 'echo -e "[azure-cli]\nname=Azure CLI\nbaseurl=https://packages.microsoft.com/yumrepos/azure-cli\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/azure-cli.repo'
+    sudo yum install azure-cli
+    
+    # Install PowerShell Core
+    sudo yum install -y powershell
+    
+    # Install .NET Core - CentOS
+    sudo rpm -Uvh https://packages.microsoft.com/config/rhel/7/packages-microsoft-prod.rpm
+    sudo yum update
+    sudo yum install dotnet-sdk-2.2
+    
+    # Install NuGet
+    yum install nuget
+    
+fi
+
+# Check for Debian-based distro
+if [ -f /etc/debian_version ]; then
+    
+    # Install Azure CLI
+    curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+    
+    # Install PowerShell Core
+    sudo apt-get install -y powershell
+    
+    # Install .NET Core - Ubuntu 18.04
+    wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb
+    sudo dpkg -i packages-microsoft-prod.deb
+    sudo add-apt-repository universe
+    sudo apt-get install apt-transport-https
+    sudo apt-get update
+    sudo apt-get install dotnet-sdk-2.2
+    
+    # Install NuGet
+    sudo apt-get install nuget -y
+    
+fi
