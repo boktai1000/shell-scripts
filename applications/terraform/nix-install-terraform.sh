dc7ebe1175bbaeace3f50d46c315369d59d55cc2
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-06-13 10:15:29 -0500

    Update nix-install-terraform.sh

--- applications/terraform/nix-install-terraform.sh
+++ applications/terraform/nix-install-terraform.sh
@@ -4,5 +4,22 @@
 # curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/applications/terraform/nix-install-terraform.sh | sudo bash
 # curl -s https://bitbucket.org/boktai1000/shell-scripts/raw/master/applications/terraform/nix-install-terraform.sh | sudo bash
 
-# Set Variable for your IP Address
-yourip=$(hostname -I | awk '{print $1}')
+# Set variables
+TERRAFORM_VERSION=${1:-0.11.11}
+TERRAFORM_FILENAME=terraform_$TERRAFORM_VERSION_linux_amd64.zip
+TERRAFORM_DOWNLOAD_ADDRESS=https://releases.hashicorp.com/terraform/$TERRAFORM_VERSION/$TERRAFORM_FILENAME
+
+# Install Unzip
+sudo apt-get install unzip -y
+
+# Download latest version of the terraform
+(cd /tmp && curl -O $TERRAFORM_DOWNLOAD_ADDRESS)
+
+# Extract the downloaded file archive
+unzip /tmp/$TERRAFORM_FILENAME
+
+# Move the executable into a directory searched for executables
+sudo mv /tmp/terraform /usr/local/bin/
+
+# Run it
+terraform --version
