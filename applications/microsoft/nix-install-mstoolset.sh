aaf9093dcc3ee2f8de39368793c8b231a12bc008
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-06-13 15:38:26 -0500

    Update nix-install-mstoolset.sh

--- applications/microsoft/nix-install-mstoolset.sh
+++ applications/microsoft/nix-install-mstoolset.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # You can run this script directly with either of the following commands
-# curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/applications/terraform/nix-install-terraform.sh | sudo bash
-# curl -s https://bitbucket.org/boktai1000/shell-scripts/raw/master/applications/terraform/nix-install-terraform.sh | sudo bash
+# curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/applications/microsoft/nix-install-mstoolset.sh | sudo bash
+# curl -s https://bitbucket.org/boktai1000/shell-scripts/raw/master/applications/microsoft/nix-install-mstoolset.sh | sudo bash
 
 # Check for Red Hat-based distro
 if [ -f /etc/redhat-release ]; then
