78cbc1ce7f5563fda2a123132f3d04f9e326f197
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-07-09 16:20:25 -0500

    Update nix-color-test.sh

--- testing/nix-color-test.sh
+++ testing/nix-color-test.sh
@@ -2,6 +2,7 @@
 
 # You can run this script directly with the following command
 # curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/testing/nix-color-test.sh | sudo bash
+# curl -s https://bitbucket.org/boktai1000/shell-scripts/raw/master/testing/nix-color-test.sh | sudo bash
 
 # https://askubuntu.com/questions/821157/print-a-256-color-test-pattern-in-the-terminal 
 curl -s https://gist.githubusercontent.com/HaleTom/89ffe32783f89f403bba96bd7bcd1263/raw/ | bash
