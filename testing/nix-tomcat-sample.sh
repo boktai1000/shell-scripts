65ae0f98285505668b495ed2d78c5b1a33210788
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2019-07-09 16:18:10 -0500

    Update nix-tomcat-sample.sh

--- testing/nix-tomcat-sample.sh
+++ testing/nix-tomcat-sample.sh
@@ -2,8 +2,8 @@
 
 # https://tomcat.apache.org/tomcat-7.0-doc/appdev/sample/
 
-# curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/applications/testing/nix-tomcat-sample.sh | sudo bash
-# curl -s https://bitbucket.org/boktai1000/shell-scripts/raw/master/applications/testing/nix-tomcat-sample.sh | sudo bash
+# curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/testing/nix-tomcat-sample.sh | sudo bash
+# curl -s https://bitbucket.org/boktai1000/shell-scripts/raw/master/testing/nix-tomcat-sample.sh | sudo bash
 
 # Download sample.war to /tmp
 (cd /tmp && curl -O https://tomcat.apache.org/tomcat-7.0-doc/appdev/sample/sample.war)
