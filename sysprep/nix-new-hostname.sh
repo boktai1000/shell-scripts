683528aa9bdbad2ffbd4ff21a5269483fc22e66e
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2020-10-02 15:51:17 -0500

    Update nix-new-hostname.sh

--- sysprep/nix-new-hostname.sh
+++ sysprep/nix-new-hostname.sh
@@ -1,2 +1,9 @@
 # https://linuxize.com/post/how-to-change-hostname-on-ubuntu-18-04/
 # https://www.cyberciti.biz/faq/ubuntu-18-04-lts-change-hostname-permanently/
+
+# Update Hostname
+# sudo hostnamectl set-hostname HOST_NAME_HERE
+
+# Update /etc/hosts file
+# Remove old 127.0.0.1 entry and replace with new hostname
+# vi /etc/hosts
