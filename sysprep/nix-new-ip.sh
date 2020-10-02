20b9aff3f78f644d7725af518a0eb6275ed297bd
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2020-10-02 16:10:46 -0500

    Update nix-new-ip.sh

--- sysprep/nix-new-ip.sh
+++ sysprep/nix-new-ip.sh
@@ -2,3 +2,33 @@
 # https://www.howtoforge.com/linux-basics-set-a-static-ip-on-ubuntu
 # vi /etc/netplan/50-cloud-init.yaml
 # vi /etc/netplan/*.yaml
+
+# Script running order: ./nix-new-ip.sh -s yourip yourgateway yourdns1 yourdns2 yourdomain
+
+yourip={1:-192.168.1.117/24}
+yourgateway={2:-192.168.1.1}
+yourdns1={3:-192.168.1.1}
+yourdns2={4:-192.168.1.1}
+yourdomain={5:-WORKGROUP}
+
+# Backup original file
+cp /etc/netplan/50-cloud-init.yaml /etc/netplan/50-cloud-init.yaml.bak-"$(date --utc +%FT%T.%3NZ)"
+
+echo "# This file is generated from information provided by the datasource.  Changes
+# to it will not persist across an instance reboot.  To disable cloud-init's
+# network configuration capabilities, write a file
+# /etc/cloud/cloud.cfg.d/99-disable-network-config.cfg with the following:
+# network: {config: disabled}
+network:
+    ethernets:
+        ens160:
+            addresses:
+            - $yourip
+            gateway4: $yourgateway
+            nameservers:
+                addresses:
+                - $yourdns1
+                - $yourdns2
+                search:
+                - $yourdomain
+    version: 2" > /etc/netplan/50-cloud-init.yaml
