2534683514f9a7b1d161c2a02dd4491b96ca333f
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2020-10-02 15:49:16 -0500

    Update nix-new-ip.sh

--- sysprep/nix-new-ip.sh
+++ sysprep/nix-new-ip.sh
@@ -1,2 +1,4 @@
 # https://askubuntu.com/questions/1036676/how-to-set-static-ip-via-script-on-ubuntu-18-04
 # https://www.howtoforge.com/linux-basics-set-a-static-ip-on-ubuntu
+# vi /etc/netplan/50-cloud-init.yaml
+# vi /etc/netplan/*.yaml
