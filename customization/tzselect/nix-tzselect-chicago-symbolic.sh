2814cc63a11652777781ac2537e2dd147eb0d0ac
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2020-11-16 15:34:12 -0600

    Update nix-tzselect-chicago-symbolic.sh

--- customization/tzselect/nix-tzselect-chicago-symbolic.sh
+++ customization/tzselect/nix-tzselect-chicago-symbolic.sh
@@ -1,5 +1,10 @@
-# Update to Central US
+# Update to Central US - Chicago
 sudo ln -s /usr/share/zoneinfo/America/Chicago /etc/localtime
 
-# Undo to UTC
+# Undo to UTC - Verified on Ubuntu 18.04 Server
+# sudo unlink /etc/localtime
 # sudo ln -s /usr/share/zoneinfo/Etc/UTC /etc/localtime
+
+# Additional References
+# https://unix.stackexchange.com/questions/297147/why-is-etc-localtime-a-symbolic-link
+# https://linuxize.com/post/how-to-remove-symbolic-links-in-linux/
