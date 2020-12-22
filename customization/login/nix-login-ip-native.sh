39f6ceac58545c8ff74c9daede4acf2cb516cf19
Keegan Jacobson <9054563+boktai1000@users.noreply.github.com>
2020-12-22 10:24:22 -0600

    Update nix-login-ip-native.sh

--- customization/login/nix-login-ip-native.sh
+++ customization/login/nix-login-ip-native.sh
@@ -3,7 +3,7 @@
 # curl -s https://raw.githubusercontent.com/boktai1000/shell-scripts/master/customization/login/nix-login-ip-native.sh | sudo bash
 
 # Backup /etc/issue
-sudo cp /etc/issue /etc/issue.bak
+sudo cp /etc/issue /etc/issue.bak-"$(date --utc +%FT%T.%3NZ)"
 
 # Append Local IP Address to /etc/issue file
 sudo tee -a /etc/issue <<EOF
